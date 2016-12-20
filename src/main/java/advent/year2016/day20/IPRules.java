462e85aa0a79cfd6cbc754ad4735204b44edd461
Sam Lindsay-Levine <sam.lindsay.levine@gmail.com>
2016-12-20 08:27:53 -0600

    Day 20, IPRules, part 2: merged ranges, counts.

--- src/main/java/advent/year2016/day20/IPRules.java
+++ src/main/java/advent/year2016/day20/IPRules.java
@@ -1,5 +1,6 @@
 package advent.year2016.day20;
 
+import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 
@@ -7,14 +8,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collector;
 import java.util.stream.Stream;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.DiscreteDomain;
 import com.google.common.collect.Range;
 
 public class IPRules {
@@ -22,11 +27,22 @@ public class IPRules {
 	private static final Pattern RANGE_PATTERN = Pattern.compile("(\\d+)-(\\d+)");
 
 	private final long maxValue;
-	private final Set<Range<Long>> blockedRanges;
+	/**
+	 * Guaranteed to have no overlapping ranges and to be sorted in ascending
+	 * order.
+	 * 
+	 * Because we canonicize them, they are also guaranteed to be [closed,
+	 * open).
+	 */
+	private final List<Range<Long>> blockedRanges;
 
 	public IPRules(long maxValue, Stream<String> blacklist) {
 		this.maxValue = maxValue;
-		this.blockedRanges = blacklist.map(IPRules::toRange).collect(toSet());
+		this.blockedRanges = blacklist.map(IPRules::toRange) //
+				.collect(toMergedRanges()) //
+				.stream() //
+				.sorted(comparing(Range::lowerEndpoint)) //
+				.collect(toList());
 	}
 
 	private static Range<Long> toRange(String rangeStr) {
@@ -34,37 +50,56 @@ public class IPRules {
 		Preconditions.checkArgument(matcher.matches(), "Bad range string %s", rangeStr);
 		long lower = Long.valueOf(matcher.group(1));
 		long upper = Long.valueOf(matcher.group(2));
-		return Range.closed(lower, upper);
+		return Range.closed(lower, upper).canonical(DiscreteDomain.longs());
 	}
 
-	private boolean isValid(long ip) {
-		return !blockedRanges.parallelStream() //
-				.anyMatch(range -> range.contains(ip));
+	@VisibleForTesting
+	static <T extends Comparable<T>> Collector<Range<T>, ?, Set<Range<T>>> toMergedRanges() {
+		return Collector.of(HashSet::new, IPRules::mergeRange, (l, r) -> {
+			r.forEach(range -> mergeRange(l, range));
+			return l;
+		});
 	}
 
-	// This approach is far too slow.
-	// public OptionalLong naiveLowestValid() {
-	// return LongStream.range(0, maxValue) //
-	// .filter(this::isValid) //
-	// .findFirst();
-	// }
+	private static <T extends Comparable<T>> void mergeRange(Set<Range<T>> existing, Range<T> toAdd) {
+		Set<Range<T>> overlaps = existing.stream() //
+				.filter(toAdd::isConnected) //
+				.collect(toSet());
+
+		existing.removeAll(overlaps);
+
+		Range<T> merged = overlaps.stream() //
+				.reduce(toAdd, Range::span);
+
+		existing.add(merged);
+	}
 
 	public Optional<Long> lowestValid() {
+
 		if (blockedRanges.isEmpty()) {
 			return Optional.of(0L);
 		}
 
-		// We'll test each point that is one below a lower bound of one of our
-		// blocked ranges, and see if it is valid.
-		List<Long> possiblyValid = blockedRanges.stream() //
-				.map(Range::lowerEndpoint) //
-				.map(i -> i - 1) //
-				.filter(i -> i >= 0 && i <= maxValue).sorted() //
-				.collect(toList());
+		Range<Long> first = this.blockedRanges.iterator().next();
+		if (first.lowerEndpoint() > 0) {
+			return Optional.of(0L);
+		} else if (first.upperEndpoint() > maxValue) {
+			return Optional.empty();
+		} else {
+			// If the blocked range is [0, 7) then 7 is allowed.
+			return Optional.of(first.upperEndpoint());
+		}
+	}
 
-		return possiblyValid.stream() //
-				.filter(this::isValid) //
-				.findFirst();
+	public long validCount() {
+		long bannedCount = blockedRanges.parallelStream() //
+				// e.g., the range [0, 2) blocks 2 values, 0 and 1.
+				.mapToLong(r -> r.upperEndpoint() - r.lowerEndpoint()) //
+				.sum();
+
+		long totalNumbers = maxValue + 1;
+
+		return totalNumbers - bannedCount;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -75,6 +110,7 @@ public class IPRules {
 		try (Stream<String> lines = Files.lines(inputFilePath)) {
 			IPRules rules = new IPRules(maxValue, lines);
 			System.out.println(rules.lowestValid());
+			System.out.println(rules.validCount());
 		}
 
 	}
--- src/test/java/advent/year2016/day20/IPRulesTest.java
+++ src/test/java/advent/year2016/day20/IPRulesTest.java
@@ -3,9 +3,13 @@ package advent.year2016.day20;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import org.junit.Test;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Range;
 
 public class IPRulesTest {
 
@@ -19,6 +23,50 @@ public class IPRulesTest {
 
 		IPRules rules = new IPRules(maxValue, blacklist);
 		assertEquals(Optional.of(3L), rules.lowestValid());
+
+		assertEquals(2L, rules.validCount());
+	}
+
+	@Test
+	public void toMergedRangesDisjoint() {
+		Stream<Range<Integer>> ranges = Stream.of( //
+				Range.closed(1, 2), //
+				Range.closed(4, 5), //
+				Range.closed(7, 8));
+
+		Set<Range<Integer>> expected = ImmutableSet.of(//
+				Range.closed(1, 2), //
+				Range.closed(4, 5), //
+				Range.closed(7, 8));
+
+		assertEquals(expected, ranges.collect(IPRules.toMergedRanges()));
+	}
+
+	@Test
+	public void toMergedRangesSingleOverlap() {
+		Stream<Range<Integer>> ranges = Stream.of( //
+				Range.closed(1, 4), //
+				Range.closed(4, 5), //
+				Range.closed(7, 8));
+
+		Set<Range<Integer>> expected = ImmutableSet.of(//
+				Range.closed(1, 5), //
+				Range.closed(7, 8));
+
+		assertEquals(expected, ranges.collect(IPRules.toMergedRanges()));
+	}
+
+	@Test
+	public void toMergedRangesAllOverlap() {
+		Stream<Range<Integer>> ranges = Stream.of( //
+				Range.closed(1, 4), //
+				Range.closed(3, 7), //
+				Range.closed(6, 8));
+
+		Set<Range<Integer>> expected = ImmutableSet.of(//
+				Range.closed(1, 8));
+
+		assertEquals(expected, ranges.collect(IPRules.toMergedRanges()));
 	}
 
 }
