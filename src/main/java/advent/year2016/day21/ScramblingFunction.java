9796437d4008a234b954dee243a1f9103e67d4cb
Sam Lindsay-Levine <sam.lindsay.levine@gmail.com>
2016-12-21 07:43:10 -0600

    Day 21, part 1: Scrambling.

---
+++ src/main/java/advent/year2016/day21/ScramblingFunction.java
@@ -0,0 +1,174 @@
+package advent.year2016.day21;
+
+import static java.util.stream.Collectors.toList;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import com.google.common.base.Preconditions;
+
+public class ScramblingFunction {
+
+	private final List<ScramblingInstruction> instructions;
+
+	public ScramblingFunction(Stream<String> lines) {
+		this.instructions = lines.map(ScramblingInstruction::of).collect(toList());
+	}
+
+	public String apply(String input) {
+		String output = input;
+
+		for (ScramblingInstruction instruction : instructions) {
+			output = instruction.apply(output);
+		}
+
+		return output;
+	}
+
+	static class ScramblingInstruction {
+		private final ScramblingOperation op;
+		private final Matcher matcher;
+
+		private ScramblingInstruction(ScramblingOperation op, Matcher matcher) {
+			this.op = op;
+			Preconditions.checkArgument(matcher.matches());
+			this.matcher = matcher;
+		}
+
+		public static ScramblingInstruction of(String input) {
+			return Arrays.stream(ScramblingOperation.values()) //
+					.filter(op -> op.pattern.matcher(input).matches()) //
+					.map(op -> new ScramblingInstruction(op, op.pattern.matcher(input))) //
+					.findFirst() //
+					.orElseThrow(() -> new IllegalArgumentException("Unrecognized instruction " + input));
+		}
+
+		public int getInt(int oneBasedIndex) {
+			return Integer.parseInt(this.getString(oneBasedIndex));
+		}
+
+		public String getString(int oneBasedIndex) {
+			return matcher.group(oneBasedIndex);
+		}
+
+		public String apply(String input) {
+			return op.apply(input, this);
+		}
+	}
+
+	private static enum ScramblingOperation {
+		SWAP_POSITION("swap position (\\d+) with position (\\d+)") {
+			@Override
+			public String apply(String input, ScramblingInstruction instruction) {
+				return swap(input, instruction.getInt(1), instruction.getInt(2));
+			}
+		}, //
+
+		SWAP_LETTER("swap letter (\\w+) with letter (\\w+)") {
+			@Override
+			public String apply(String input, ScramblingInstruction instruction) {
+				int indexOne = input.indexOf(instruction.getString(1));
+				int indexTwo = input.indexOf(instruction.getString(2));
+				return swap(input, indexOne, indexTwo);
+			}
+		}, //
+
+		ROTATE_LEFT("rotate left (\\d+) steps?") {
+			@Override
+			public String apply(String input, ScramblingInstruction instruction) {
+				return rotateLeft(input, instruction.getInt(1));
+			}
+		}, //
+
+		ROTATE_RIGHT("rotate right (\\d+) steps?") {
+			@Override
+			public String apply(String input, ScramblingInstruction instruction) {
+				return rotateRight(input, instruction.getInt(1));
+			}
+		}, //
+
+		ROTATE_BASED_ON_LETTER("rotate based on position of letter (\\w+)") {
+			@Override
+			public String apply(String input, ScramblingInstruction instruction) {
+				int index = input.indexOf(instruction.getString(1));
+				int steps = 1 + index + (index >= 4 ? 1 : 0);
+				return rotateRight(input, steps);
+			}
+		}, //
+
+		REVERSE_POSITIONS("reverse positions (\\d+) through (\\d+)") {
+			@Override
+			public String apply(String input, ScramblingInstruction instruction) {
+				int min = instruction.getInt(1);
+				int max = instruction.getInt(2);
+
+				return input.substring(0, min) + //
+				reverse(input.substring(min, max + 1)) + //
+				input.substring(max + 1);
+			}
+		}, //
+
+		MOVE_POSITION("move position (\\d+) to position (\\d+)") {
+			@Override
+			public String apply(String input, ScramblingInstruction instruction) {
+				int source = instruction.getInt(1);
+				int target = instruction.getInt(2);
+				if (source < target) {
+					return input.substring(0, source) + input.substring(source + 1, target + 1) + input.charAt(source)
+							+ input.substring(target + 1);
+				} else {
+					return input.substring(0, target) + input.charAt(source) + input.substring(target, source)
+							+ input.substring(source + 1);
+				}
+			}
+		}; //
+
+		private final Pattern pattern;
+
+		ScramblingOperation(String regex) {
+			this.pattern = Pattern.compile(regex);
+		}
+
+		public abstract String apply(String input, ScramblingInstruction instruction);
+	}
+
+	private static String swap(String input, int indexOne, int indexTwo) {
+		int min = Math.min(indexOne, indexTwo);
+		int max = Math.max(indexOne, indexTwo);
+
+		return input.substring(0, min) + //
+				input.charAt(max) + //
+				input.substring(min + 1, max) + //
+				input.charAt(min) + //
+				input.substring(max + 1);
+	}
+
+	private static String rotateLeft(String input, int steps) {
+		int effectiveSteps = Math.floorMod(steps, input.length());
+		return input.substring(effectiveSteps) + input.substring(0, effectiveSteps);
+	}
+
+	private static String rotateRight(String input, int steps) {
+		return rotateLeft(input, -steps);
+	}
+
+	private static String reverse(String input) {
+		return new StringBuilder(input).reverse().toString();
+	}
+
+	public static void main(String[] args) throws IOException {
+		Path inputFilePath = Paths.get("src/main/java/advent/year2016/day21/input.txt");
+
+		try (Stream<String> lines = Files.lines(inputFilePath)) {
+			ScramblingFunction function = new ScramblingFunction(lines);
+			System.out.println(function.apply("abcdefgh"));
+		}
+	}
+}
---
+++ src/test/java/advent/year2016/day21/ScramblingFunctionTest.java
@@ -0,0 +1,92 @@
+package advent.year2016.day21;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+import advent.year2016.day21.ScramblingFunction.ScramblingInstruction;
+
+public class ScramblingFunctionTest {
+
+	@Test
+	public void swapNumbers() {
+		String input = "abcde";
+		String output = ScramblingInstruction.of("swap position 4 with position 0").apply(input);
+		String expected = "ebcda";
+		assertEquals(expected, output);
+	}
+
+	@Test
+	public void swapLetters() {
+		String input = "ebcda";
+		String output = ScramblingInstruction.of("swap letter d with letter b").apply(input);
+		String expected = "edcba";
+		assertEquals(expected, output);
+	}
+
+	@Test
+	public void reversePositions() {
+		String input = "edcba";
+		String output = ScramblingInstruction.of("reverse positions 0 through 4").apply(input);
+		String expected = "abcde";
+		assertEquals(expected, output);
+	}
+
+	@Test
+	public void rotateLeft() {
+		String input = "abcde";
+		String output = ScramblingInstruction.of("rotate left 1 step").apply(input);
+		String expected = "bcdea";
+		assertEquals(expected, output);
+	}
+
+	@Test
+	public void movePosition1to4() {
+		String input = "bcdea";
+		String output = ScramblingInstruction.of("move position 1 to position 4").apply(input);
+		String expected = "bdeac";
+		assertEquals(expected, output);
+	}
+
+	@Test
+	public void movePosition3to0() {
+		String input = "bdeac";
+		String output = ScramblingInstruction.of("move position 3 to position 0").apply(input);
+		String expected = "abdec";
+		assertEquals(expected, output);
+	}
+
+	@Test
+	public void rotateNoBonus() {
+		String input = "abdec";
+		String output = ScramblingInstruction.of("rotate based on position of letter b").apply(input);
+		String expected = "ecabd";
+		assertEquals(expected, output);
+	}
+
+	@Test
+	public void rotateWithBonus() {
+		String input = "abdec";
+		String output = ScramblingInstruction.of("rotate based on position of letter d").apply(input);
+		String expected = "decab";
+		assertEquals(expected, output);
+	}
+
+	@Test
+	public void wholeFunction() {
+		Stream<String> lines = Stream.of("swap position 4 with position 0", //
+				"swap letter d with letter b", //
+				"reverse positions 0 through 4", //
+				"rotate left 1 step", //
+				"move position 1 to position 4", //
+				"move position 3 to position 0", //
+				"rotate based on position of letter b", //
+				"rotate based on position of letter d");
+
+		ScramblingFunction function = new ScramblingFunction(lines);
+
+		assertEquals("decab", function.apply("abcde"));
+	}
+}
