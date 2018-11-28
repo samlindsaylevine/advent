53f0bc0bdee3a32a746bc9500653a3023cbd9d7f
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2018-11-28 15:49:59 -0600

    Day 21, part 1: FractalArt.

---
+++ src/main/kotlin/advent/year2017/day21/FractalArt.kt
@@ -0,0 +1,113 @@
+package advent.year2017.day21
+
+import java.io.File
+
+class FractalArtBook(private val rules: List<FractalArtRule>) {
+    constructor(input: String) : this(input.trim().split("\n")
+            .map { FractalArtRule.fromLine(it) })
+
+    fun next(image: FractalArtImage): FractalArtImage {
+        val partitioned = image.partition()
+
+        return partitioned.map { it.map { square -> nextSquare(square) } }
+                .merge()
+    }
+
+    /**
+     * Finds the next iteration for a single partitioned square, checking it against the rules.
+     */
+    private fun nextSquare(square: FractalArtImage): FractalArtImage {
+        val keys = square.equivalentRuleKeys()
+
+        val rule = rules.firstOrNull() { rule -> square.equivalentRuleKeys().contains(rule.from) }
+                ?: throw IllegalArgumentException("Could not find rule from any key $keys")
+
+        return rule.to
+    }
+}
+
+data class FractalArtRule(val from: FractalArtImage, val to: FractalArtImage) {
+    companion object {
+        private val REGEX = "(.*) => (.*)".toRegex()
+        fun fromLine(line: String): FractalArtRule {
+            val match = REGEX.matchEntire(line.trim()) ?: throw IllegalArgumentException("Unparseable rule $line")
+            return FractalArtRule(FractalArtImage(match.groupValues[1], lineDelimiter = "/"),
+                    FractalArtImage(match.groupValues[2], lineDelimiter = "/"))
+        }
+    }
+}
+
+data class FractalArtImage internal constructor(val pixels: List<List<Boolean>>) {
+    constructor(pattern: String, lineDelimiter: String = "\n") : this(pattern.trim().split(lineDelimiter)
+            .map { line -> line.toCharArray().map { it == '#' }.toList() })
+
+    override fun toString() = pixels.joinToString(separator = "\n")
+    { line -> line.joinToString(separator = "") { if (it) "#" else "." } }
+
+    fun onPixelCount() = pixels.asSequence()
+            .flatMap { it.asSequence() }
+            .count { it }
+
+    fun partition(): List<List<FractalArtImage>> {
+        val size = pixels.size
+        val squareSize = if (size % 2 == 0) 2 else 3
+        val numSquares = size / squareSize
+
+        return (0 until numSquares).map { y ->
+            (0 until numSquares).map { x -> square(x, y, squareSize) }
+        }
+    }
+
+    private fun square(squareX: Int, squareY: Int, size: Int): FractalArtImage {
+        val minX = squareX * size
+        val minY = squareY * size
+
+        val squarePixels = (minY until minY + size).map { y ->
+            (minX until minX + size).map { x -> pixels[y][x] }
+        }
+
+        return FractalArtImage(squarePixels)
+    }
+
+    fun flipped(): FractalArtImage = FractalArtImage(this.pixels.reversed())
+    fun rotated(): FractalArtImage = FractalArtImage((0 until pixels.size).map { y ->
+        (0 until pixels.size).map { x -> pixels[pixels.size - 1 - x][y] }
+    })
+
+    fun equivalentRuleKeys() = listOf(this,
+            this.rotated(),
+            this.rotated().rotated(),
+            this.rotated().rotated().rotated(),
+            this.flipped(),
+            this.flipped().rotated(),
+            this.flipped().rotated().rotated(),
+            this.flipped().rotated().rotated().rotated())
+}
+
+
+fun List<List<FractalArtImage>>.merge(): FractalArtImage {
+    return FractalArtImage(this.flatMap { it.merge() })
+}
+
+private fun List<FractalArtImage>.merge(): List<List<Boolean>> {
+    if (this.isEmpty()) return listOf()
+
+    val size = this.first().pixels.size
+    return (0 until size).map { rowNum -> this.flatMap { image -> image.pixels[rowNum] } }
+}
+
+fun main(args: Array<String>) {
+    val input = File("src/main/kotlin/advent/year2017/day21/input.txt")
+            .readText()
+
+    val book = FractalArtBook(input)
+
+    val starterImage = FractalArtImage("""
+            .#.
+            ..#
+            ###
+        """.trimIndent())
+
+    val finalImage = (1..5).fold(starterImage) { image, i -> book.next(image) }
+    println(finalImage.onPixelCount())
+}
---
+++ src/test/kotlin/advent/year2017/day21/FractalArtTest.kt
@@ -0,0 +1,197 @@
+package advent.year2017.day21
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import kotlin.math.exp
+
+class FractalArtTest {
+
+    private val starterImage = """
+            .#.
+            ..#
+            ###
+        """.trimIndent()
+
+    private val referenceBook = """
+        ../.# => ##./#../...
+        .#./..#/### => #..#/..../..../#..#
+    """.trimIndent()
+
+    @Test
+    fun `parse and toString -- starter image -- yields original`() {
+        val image = FractalArtImage(starterImage)
+
+        val string = image.toString()
+
+        assertThat(string).isEqualTo(starterImage)
+    }
+
+    @Test
+    fun `onPixelCount -- reference image -- 5 pixels`() {
+        val image = FractalArtImage(starterImage)
+
+        val count = image.onPixelCount()
+
+        assertThat(count).isEqualTo(5)
+    }
+
+    @Test
+    fun `next -- reference book & start -- reference 1st iteration result`() {
+        val book = FractalArtBook(referenceBook)
+        val start = FractalArtImage(starterImage)
+
+        val next = book.next(start)
+
+        val expected = FractalArtImage("""
+            #..#
+            ....
+            ....
+            #..#
+        """.trimIndent())
+        assertThat(next).isEqualTo(expected)
+    }
+
+    @Test
+    fun `next -- reference book & 1st iteration -- reference 2nd iteration result`() {
+        val book = FractalArtBook(referenceBook)
+        val first = FractalArtImage("""
+            #..#
+            ....
+            ....
+            #..#
+        """.trimIndent())
+
+        val second = book.next(first)
+
+        val expected = FractalArtImage("""
+            ##.##.
+            #..#..
+            ......
+            ##.##.
+            #..#..
+            ......
+        """.trimIndent())
+        assertThat(second).isEqualTo(expected)
+        assertThat(second.onPixelCount()).isEqualTo(12)
+    }
+
+    @Test
+    fun `partition -- reference image -- reference partition result`() {
+        val image = FractalArtImage("""
+            #..#
+            ....
+            ....
+            #..#
+        """.trimIndent())
+
+        val partition = image.partition()
+
+        val expected = listOf(
+                listOf(
+                        FractalArtImage("""
+                            #.
+                            ..
+                            """.trimIndent()),
+                        FractalArtImage("""
+                            .#
+                            ..
+                            """.trimIndent())),
+                listOf(
+                        FractalArtImage("""
+                            ..
+                            #.
+                            """.trimIndent()),
+                        FractalArtImage("""
+                            ..
+                            .#
+                            """.trimIndent())))
+        assertThat(partition).isEqualTo(expected)
+    }
+
+    @Test
+    fun `merge -- reference merge input -- reference merge result`() {
+        val input = listOf(
+                listOf(
+                        FractalArtImage("""
+                            ##.
+                            #..
+                            ...
+                            """.trimIndent()),
+                        FractalArtImage("""
+                            ##.
+                            #..
+                            ...
+                            """.trimIndent())),
+                listOf(
+                        FractalArtImage("""
+                            ##.
+                            #..
+                            ...
+                            """.trimIndent()),
+                        FractalArtImage("""
+                            ##.
+                            #..
+                            ...
+                            """.trimIndent())))
+
+        val merged = input.merge()
+
+        val expected = FractalArtImage("""
+            ##.##.
+            #..#..
+            ......
+            ##.##.
+            #..#..
+            ......
+        """.trimIndent())
+        assertThat(merged).isEqualTo(expected)
+    }
+
+    @Test
+    fun `flipped -- a normal image -- is vertically flipped`() {
+        val input = FractalArtImage("""
+            ##.###
+            #....#
+            ......
+            #..##.
+            #....#
+            ......
+        """.trimIndent())
+
+        val flipped = input.flipped()
+
+        val expected = FractalArtImage("""
+            ......
+            #....#
+            #..##.
+            ......
+            #....#
+            ##.###
+        """.trimIndent())
+        assertThat(flipped).isEqualTo(expected)
+    }
+
+    @Test
+    fun `rotated -- a normal image -- is rotated a quarter turn clockwise`() {
+        val input = FractalArtImage("""
+            ##.###
+            #....#
+            ......
+            #..##.
+            #....#
+            ......
+        """.trimIndent())
+
+        val rotated = input.rotated()
+
+        val expected = FractalArtImage("""
+            .##.##
+            .....#
+            ......
+            ..#..#
+            ..#..#
+            .#..##
+        """.trimIndent())
+        assertThat(rotated).isEqualTo(expected)
+    }
+}
