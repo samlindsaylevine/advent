174ac1b3e8d1d7103b169ca75588a25dfd690735
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2018-11-28 14:05:57 -0600

    Day 20: ParticleSwarm survivor count.

--- src/main/kotlin/advent/year2017/day20/ParticleSwarm.kt
+++ src/main/kotlin/advent/year2017/day20/ParticleSwarm.kt
@@ -2,11 +2,12 @@ package advent.year2017.day20
 
 import java.io.File
 import java.lang.IllegalArgumentException
+import kotlin.math.sign
 
-class ParticleSwarm(text: String) {
+class ParticleSwarm(private val particles: List<Particle>) {
 
-    private val particles: List<Particle> = text.trim().split("\n")
-            .map { Particle.fromLine(it) }
+    constructor(text: String) : this(text.trim().split("\n")
+            .map { Particle.fromLine(it) })
 
     /**
      * @return the index in the array of the particle that remains closest to the origin (by Manhattan distance)
@@ -22,6 +23,40 @@ class ParticleSwarm(text: String) {
                     Math.abs(particles[i].y.acceleration) +
                     Math.abs(particles[i].z.acceleration)
         }
+    }
+
+    /**
+     * How many particles are left after all collisions have taken place.
+     */
+    fun survivorCount(): Int = generateSequence(this, ParticleSwarm::next)
+            .filter { it.willNeverHaveAnyMoreCollisions() }
+            .first()
+            .particles.size
+
+    private fun withoutCollidingParticles(): ParticleSwarm {
+        val collisions = collidingParticles()
+        return ParticleSwarm(particles.filter { !collisions.contains(it) })
+    }
+
+    fun collidingParticles(): Set<Particle> = allPairs()
+            .filter { it.first.collidesWith(it.second) }
+            .flatMap { sequenceOf(it.first, it.second) }
+            .toSet()
+
+    private fun allPairs(): Sequence<Pair<Particle, Particle>> = particles.indices.asSequence()
+            .flatMap { i -> particles.indices.asSequence().filter { j -> j != i }.map { j -> Pair(i, j) } }
+            .map { Pair(particles[it.first], particles[it.second]) }
+
+    fun next(): ParticleSwarm {
+        return ParticleSwarm(this.withoutCollidingParticles()
+                .particles.map { it.next() })
+    }
+
+    private fun willNeverHaveAnyMoreCollisions(): Boolean {
+        val sortedByXAccel = particles.sortedBy { it.x.acceleration }
+        val pairs = sortedByXAccel.zipWithNext()
+
+        return pairs.all { it.first.willNotEverCollide(it.second) }
     }
 }
 
@@ -49,11 +84,37 @@ data class Particle(val x: AxisTrajectory,
         }
     }
 
+    fun willNotEverCollide(other: Particle) = this.x.willNotEverCollide(other.x) ||
+            this.y.willNotEverCollide(other.y) ||
+            this.z.willNotEverCollide(other.z)
+
+    fun next() = Particle(x.next(), y.next(), z.next())
+
+    fun collidesWith(other: Particle) = this.x.position == other.x.position &&
+            this.y.position == other.y.position &&
+            this.z.position == other.z.position
 }
 
-data class AxisTrajectory(val initialPosition: Int,
+data class AxisTrajectory(val position: Int,
                           val velocity: Int,
-                          val acceleration: Int)
+                          val acceleration: Int) {
+    // If the difference between acceleration, velocity, and position all have the same sign (or are
+    // 0) then we know that one value will never catch up to the other.
+    fun willNotEverCollide(other: AxisTrajectory): Boolean {
+        if (this == other) return false
+
+        val positionSign = (this.position - other.position).sign
+        val velocitySign = (this.velocity - other.velocity).sign
+        val accelerationSign = (this.acceleration - other.acceleration).sign
+
+        return (positionSign <= 0 && velocitySign <= 0 && accelerationSign <= 0) ||
+                (positionSign >= 0 && velocitySign >= 0 && accelerationSign >= 0)
+    }
+
+    fun next() = AxisTrajectory(this.position + this.velocity + this.acceleration,
+            this.velocity + this.acceleration,
+            this.acceleration)
+}
 
 fun main(args: Array<String>) {
     val input = File("src/main/kotlin/advent/year2017/day20/input.txt")
@@ -62,4 +123,5 @@ fun main(args: Array<String>) {
     val swarm = ParticleSwarm(input)
 
     println(swarm.closestToOriginIndex())
+    println(swarm.survivorCount())
 }
--- src/test/kotlin/advent/year2017/day20/ParticleSwarmTest.kt
+++ src/test/kotlin/advent/year2017/day20/ParticleSwarmTest.kt
@@ -5,6 +5,13 @@ import org.junit.jupiter.api.Test
 
 class ParticleSwarmTest {
 
+    val referenceInput = """
+            p=<-6,0,0>, v=<3,0,0>, a=<0,0,0>
+            p=<-4,0,0>, v=<2,0,0>, a=<0,0,0>
+            p=<-2,0,0>, v=<1,0,0>, a=<0,0,0>
+            p=<3,0,0>, v=<-1,0,0>, a=<0,0,0>
+        """.trimIndent()
+
     @Test
     fun `particle from line -- first line of input -- gives expected particle`() {
         val line = "p=<1609,-863,-779>, v=<-15,54,-69>, a=<-10,0,14>"
@@ -16,5 +23,23 @@ class ParticleSwarmTest {
                 AxisTrajectory(-863, 54, 0),
                 AxisTrajectory(-779, -69, 14))
         )
+    }
+
+    @Test
+    fun `survivor count -- reference input -- reference output`() {
+        val swarm = ParticleSwarm(referenceInput)
+
+        val survivors = swarm.survivorCount()
+
+        assertThat(survivors).isEqualTo(1)
+    }
+
+    @Test
+    fun `colliding particles -- initial state of reference input -- no collisions`() {
+        val swarm = ParticleSwarm(referenceInput)
+
+        val collisions = swarm.collidingParticles()
+
+        assertThat(collisions).isEmpty()
     }
 }
