c5d1c85c8e2cb5f3047e9d9af4683d1475bfc8cf
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2018-12-01 08:48:44 -0600

    Year 2018, day 1: FrequencyDrift.

---
+++ src/main/kotlin/advent/year2018/day1/FrequencyDrift.kt
@@ -0,0 +1,43 @@
+package advent.year2018.day1
+
+import com.google.common.collect.Iterators
+import java.io.File
+
+class FrequencyDrift(deltas: List<Int>) {
+
+    companion object {
+        private fun parseDelta(input: String): Int = when {
+            input.startsWith("+") -> input.substring(1).toInt()
+            else -> input.toInt()
+        }
+    }
+
+    constructor(input: String) : this(input.trim().lines().map(::parseDelta))
+
+    val result = deltas.sum()
+
+    val firstFrequencyReachedTwice by lazy {
+        val observedFrequencies = mutableSetOf<Int>()
+
+        var currentFrequency = 0
+        val loopDeltas = Iterators.cycle(deltas)
+
+        while (!(currentFrequency in observedFrequencies)) {
+            observedFrequencies.add(currentFrequency)
+            currentFrequency += loopDeltas.next()
+        }
+
+        currentFrequency
+    }
+}
+
+fun main(args: Array<String>) {
+    val input = File("src/main/kotlin/advent/year2018/day1/input.txt")
+            .readText()
+            .trim()
+
+    val drift = FrequencyDrift(input)
+
+    println(drift.result)
+    println(drift.firstFrequencyReachedTwice)
+}
---
+++ src/test/kotlin/advent/year2018/day1/FrequencyDriftTest.kt
@@ -0,0 +1,37 @@
+package advent.year2018.day1
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class FrequencyDriftTest {
+
+    @Test
+    fun `parsing -- reference input -- reference total frequency`() {
+        val input = """
+            +1
+            -2
+            +3
+            +1
+        """.trimIndent()
+
+        val result = FrequencyDrift(input).result
+
+        assertThat(result).isEqualTo(3)
+    }
+
+    @ParameterizedTest
+    @CsvSource("1;-2;3;1, 2",
+            "1;-1, 0",
+            "3;3;4;-2;-4, 10",
+            "-6;3;8;5;-6, 5",
+            "7;7;-2;-7;-4, 14")
+    fun `firstFrequencyReachedTwice -- reference inputs -- reference outputs`(input: String, expected: Int) {
+        val frequencyDrift = FrequencyDrift(input.split(";").map { it.toInt() })
+
+        val firstFrequencyReachedTwice = frequencyDrift.firstFrequencyReachedTwice
+
+        assertThat(firstFrequencyReachedTwice).isEqualTo(expected)
+    }
+}
