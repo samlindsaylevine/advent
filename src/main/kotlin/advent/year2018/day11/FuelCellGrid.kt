d87ac612b3b37a8a0393b51c8408a22d3a757622
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2018-12-14 16:23:39 -0600

    Day 11: FuelCellGrid.

--- src/main/kotlin/advent/year2018/day11/FuelCellGrid.kt
+++ src/main/kotlin/advent/year2018/day11/FuelCellGrid.kt
@@ -19,22 +19,40 @@ class FuelCellGrid(private val serialNumber: Int) {
 
     private fun Int.hundredsDigit() = (this / 100) % 10
 
-    fun upperLeftOfMostPowerfulSquare(): Coordinates = box(Coordinates(0, 0), width - 3, height - 3)
-            .maxBy { powerOfSquare(it) }
+    fun upperLeftOfMostPowerfulSquareOfSize(size: Int) = mostPowerfulSquareOfSize(size).square.upperLeft
+
+    private fun mostPowerfulSquareOfSize(size: Int): SquareAndPower = box(Coordinates(0, 0), width - size + 1, height - size + 1)
+            .map { Square(it, size) }
+            .map { SquareAndPower(it, powerOfSquare(it)) }
+            .maxBy { it.power }
             ?: throw IllegalStateException("Box too small to contain any square")
 
-    private fun powerOfSquare(upperLeft: Coordinates) = box(upperLeft, 3, 3)
-            .map { powerLevel(it) }
-            .sum()
+    fun powerOfSquare(square: Square) = box(square.upperLeft, square.size, square.size)
+            .sumBy { powerLevel(it) }
 
-    private fun box(upperLeft: Coordinates, width: Int, height: Int) = (upperLeft.x until upperLeft.x + width).asSequence().flatMap { x ->
-        (upperLeft.y until upperLeft.y + height).asSequence().map { y -> Coordinates(x, y) }
-    }
+    private fun box(upperLeft: Coordinates, width: Int, height: Int): Sequence<Coordinates> =
+            (upperLeft.x until upperLeft.x + width).asSequence().flatMap { x ->
+                (upperLeft.y until upperLeft.y + height).asSequence().map { y -> Coordinates(x, y) }
+            }
 
+    data class Square(val upperLeft: Coordinates, val size: Int)
+    private data class SquareAndPower(val square: Square, val power: Int)
+
+    fun mostPowerfulSquare(upperSearchBound: Int = width) = (1 until upperSearchBound).asSequence()
+            // for debugging progress
+            // .onEach { println(it) }
+            .map { mostPowerfulSquareOfSize(it) }
+            .maxBy { it.power }
+            ?.square
 }
 
 fun main(args: Array<String>) {
     val grid = FuelCellGrid(9005)
 
-    println(grid.upperLeftOfMostPowerfulSquare())
+    println(grid.upperLeftOfMostPowerfulSquareOfSize(3))
+
+    // We really should search all the squares up to max size, but that takes way too freaking long so I'm going to
+    // use this ugly hack and only go up to this size. I've convinced myself that larger squares do not appear to
+    // have anything higher in value.
+    println(grid.mostPowerfulSquare(upperSearchBound = 20))
 }
--- src/test/kotlin/advent/year2018/day11/FuelCellGridTest.kt
+++ src/test/kotlin/advent/year2018/day11/FuelCellGridTest.kt
@@ -2,6 +2,7 @@ package advent.year2018.day11
 
 import advent.year2018.day10.Coordinates
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -31,9 +32,24 @@ class FuelCellGridTest {
                                                                                 expectedY: Int) {
         val grid = FuelCellGrid(serialNumber)
 
-        val upperLeft = grid.upperLeftOfMostPowerfulSquare()
+        val upperLeft = grid.upperLeftOfMostPowerfulSquareOfSize(3)
 
         val expected = Coordinates(expectedX, expectedY)
         assertThat(upperLeft).isEqualTo(expected)
+    }
+
+    @ParameterizedTest(name = "mostPowerfulSquare -- grid serial number {0} -- corner {1},{2} size {3}")
+    @CsvSource("18, 90, 269, 16",
+            "42, 232, 251, 12")
+    @Disabled("so as not to take too long in CI")
+    fun `mostPowerfulSquare -- reference inputs -- reference values`(serialNumber: Int,
+                                                                     expectedX: Int,
+                                                                     expectedY: Int,
+                                                                     expectedSize: Int) {
+        val grid = FuelCellGrid(serialNumber)
+
+        val square = grid.mostPowerfulSquare(upperSearchBound = 20)
+
+        assertThat(square).isEqualTo(FuelCellGrid.Square(Coordinates(expectedX, expectedY), expectedSize))
     }
 }
