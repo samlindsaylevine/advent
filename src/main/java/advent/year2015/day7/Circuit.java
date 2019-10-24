3ad10624395c89554538a778758e80265fa6f8f3
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2019-10-23 23:02:08 -0500

    Fixes to old code after updating to JDK 11.
    One test of laziness in PackageGrouping had a comment "don't understand why this is 2 not 1", and now it is 1. Changed enforcement.
    
    One test of Circuit now revealed a ConcurrentModificationException when doing computeIfAbsent on a HashMap. It turns out in JDK 9+ the computation must not modify the map. Changed this to use a Guava LoadingCache instead.

--- src/main/java/advent/year2015/day7/Circuit.java
+++ src/main/java/advent/year2015/day7/Circuit.java
@@ -1,33 +1,43 @@
 package advent.year2015.day7;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class Circuit {
 
-	private Map<String, Gate> gatesByOutput = new HashMap<>();
+    private Map<String, Gate> gatesByOutput = new HashMap<>();
 
-	private Map<String, Integer> memoizedValues = new HashMap<>();
+    private LoadingCache<String, Integer> memoizedValues = CacheBuilder.newBuilder()
+            .build(CacheLoader.from(w -> this.gatesByOutput.get(w).get(this)));
 
-	public int get(String wire) {
-		return this.memoizedValues.computeIfAbsent(wire, w -> this.gatesByOutput.get(w).get(this));
-	}
+    public int get(String wire) {
+        try {
+            return this.memoizedValues.get(wire);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public void addGate(String representation) {
-		Gate gate = Gate.of(representation);
-		this.gatesByOutput.put(gate.getTargetWire(), gate);
-		this.memoizedValues.clear();
-	}
+    public void addGate(String representation) {
+        Gate gate = Gate.of(representation);
+        this.gatesByOutput.put(gate.getTargetWire(), gate);
+        this.memoizedValues.invalidateAll();
+    }
 
-	public static void main(String[] args) throws IOException {
-		Circuit circuit = new Circuit();
-		Files.lines(Paths.get("src/main/java/advent/year2015/day7/input.txt")) //
-				.forEach(circuit::addGate);
+    public static void main(String[] args) throws IOException {
+        Circuit circuit = new Circuit();
+        Files.lines(Paths.get("src/main/java/advent/year2015/day7/input.txt")) //
+                .forEach(circuit::addGate);
 
-		System.out.println(circuit.get("a"));
-	}
+        System.out.println(circuit.get("a"));
+    }
 
 }
--- src/test/java/advent/year2015/day24/PackageGroupingTest.java
+++ src/test/java/advent/year2015/day24/PackageGroupingTest.java
@@ -1,6 +1,7 @@
 package advent.year2015.day24;
 
 import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -18,56 +19,56 @@ import com.google.common.collect.Multiset;
 
 public class PackageGroupingTest {
 
-	@Test
-	public void powerSet() {
-		Multiset<Integer> options = ImmutableMultiset.of(1, 2, 3, 3, 4, 5);
-		List<Multiset<Integer>> powerSet = PackageGrouping.powerSet(options).collect(toList());
+    @Test
+    public void powerSet() {
+        Multiset<Integer> options = ImmutableMultiset.of(1, 2, 3, 3, 4, 5);
+        List<Multiset<Integer>> powerSet = PackageGrouping.powerSet(options).collect(toList());
 
-		assertEquals(64, powerSet.size());
-	}
+        assertEquals(64, powerSet.size());
+    }
 
-	@Test
-	public void powerSetIsLazy() {
-		AtomicInteger numSeen = new AtomicInteger(0);
+    @Test
+    public void powerSetIsLazy() {
+        AtomicInteger numSeen = new AtomicInteger(0);
 
-		List<Integer> options = IntStream.of(1, 10000).boxed().collect(toList());
-		Stream<Multiset<Integer>> powerSet = PackageGrouping.powerSet(options);
-		powerSet.peek(item -> numSeen.incrementAndGet()) //
-				.findAny();
+        List<Integer> options = IntStream.of(1, 10000).boxed().collect(toList());
+        Stream<Multiset<Integer>> powerSet = PackageGrouping.powerSet(options);
+        powerSet.peek(item -> numSeen.incrementAndGet()) //
+                .findAny();
 
-		// Not completely sure why this is 2 and not 1, but I am sure that we
-		// didn't go through all 2^10000, so I'm happy.
-		assertEquals(2, numSeen.get());
-	}
+        // On some JDK versions this is 2 and not 1.
+        // Not completely sure why. But, we didn't go through all 2^10000, so I'm happy.
+        assertThat(numSeen.get()).isLessThanOrEqualTo(2);
+    }
 
-	@Test
-	public void reference() {
-		Multiset<Integer> packages = ImmutableMultiset.of(1, 2, 3, 4, 5, 7, 8, 9, 10, 11);
+    @Test
+    public void reference() {
+        Multiset<Integer> packages = ImmutableMultiset.of(1, 2, 3, 4, 5, 7, 8, 9, 10, 11);
 
-		long bestResult = PackageGrouping.validGroupings(packages) //
-				.min(PackageGrouping.bestForSanta()) //
-				.get() //
-				.quantumEntanglementInFirstGroup();
+        long bestResult = PackageGrouping.validGroupings(packages) //
+                .min(PackageGrouping.bestForSanta()) //
+                .get() //
+                .quantumEntanglementInFirstGroup();
 
-		assertEquals(99, bestResult);
-	}
+        assertEquals(99, bestResult);
+    }
 
-	@Test
-	public void splittableEvenly() {
-		// Trivial case.
-		assertTrue(PackageGrouping.isSplittableEvenly(ImmutableList.of()));
+    @Test
+    public void splittableEvenly() {
+        // Trivial case.
+        assertTrue(PackageGrouping.isSplittableEvenly(ImmutableList.of()));
 
-		// Can't split one thing.
-		assertFalse(PackageGrouping.isSplittableEvenly(ImmutableList.of(1)));
+        // Can't split one thing.
+        assertFalse(PackageGrouping.isSplittableEvenly(ImmutableList.of(1)));
 
-		// Can be split.
-		assertTrue(PackageGrouping.isSplittableEvenly(ImmutableList.of(1, 2, 3, 4)));
+        // Can be split.
+        assertTrue(PackageGrouping.isSplittableEvenly(ImmutableList.of(1, 2, 3, 4)));
 
-		// Odd sum, not possible.
-		assertFalse(PackageGrouping.isSplittableEvenly(ImmutableList.of(1, 2, 3, 4, 5)));
+        // Odd sum, not possible.
+        assertFalse(PackageGrouping.isSplittableEvenly(ImmutableList.of(1, 2, 3, 4, 5)));
 
-		// A few larger numbers.
-		assertTrue(PackageGrouping.isSplittableEvenly(ImmutableList.of(1, 2, 3, 4, 5, 6, 19)));
-	}
+        // A few larger numbers.
+        assertTrue(PackageGrouping.isSplittableEvenly(ImmutableList.of(1, 2, 3, 4, 5, 6, 19)));
+    }
 
 }
