a49d34308f7dd3afb6714774c021d33e24a79bb3
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2019-12-04 20:13:52 -0600

    2018, Day 17: After studying another solution, converted to an imperative style for executing the flow, heavily using recursion.

--- src/main/kotlin/advent/year2018/day17/WaterReservoir.kt
+++ src/main/kotlin/advent/year2018/day17/WaterReservoir.kt
@@ -19,129 +19,11 @@ class WaterReservoir private constructor(rows: List<Row>,
     private val clay: Set<Point> = (rows.map { it.points } + columns.map { it.points })
             .fold(emptySet(), { acc, points -> acc.union(points) })
 
+    fun waterTileCount() = flowResult().waterCount()
 
-    private val minY by lazy { clay.map { it.y }.min() ?: 0 }
-    private val maxY by lazy { clay.map { it.y }.max() ?: 0 }
-    private val minX by lazy { clay.map { it.x }.min() ?: 0 }
-    private val maxX by lazy { clay.map { it.x }.max() ?: 0 }
+    fun display() = flowResult().toString()
 
-    private fun waterTiles() = waterTiles(setOf(Point(500, 0)))
-
-    fun waterTileCount() = waterTiles().all.count { it.y in minY..maxY }
-
-    private data class Point(val x: Int, val y: Int) {
-        operator fun plus(other: Point) = Point(this.x + other.x, this.y + other.y)
-    }
-
-    private data class WaterTiles(val settledWater: Set<Point>,
-                                  val flowingWater: Set<Point>) {
-        val all = settledWater + flowingWater
-    }
-
-    private tailrec fun waterTiles(springs: Set<Point>,
-                                   settledWater: Set<Point> = emptySet(),
-                                   flowingWater: Set<Point> = emptySet()): WaterTiles {
-        if (springs.isEmpty()) return WaterTiles(settledWater, flowingWater)
-
-        val spring = springs.first()
-        val otherSprings = springs - spring
-
-        val underneath = spring + Point(0, 1)
-
-        return if (spring.y >= maxY) {
-            // This spring made it to the bottom of the map and we can stop now.
-            waterTiles(otherSprings, settledWater, flowingWater + spring)
-        } else if (flowingWater.contains(underneath)) {
-            // If the spot underneath us has flowing water, we can be done with this spring -- it will join with that
-            // flow and its point is flowing water.
-            waterTiles(otherSprings, settledWater, flowingWater + spring)
-        } else if (clay.contains(underneath) || settledWater.contains(underneath)) {
-            // The water spreads out to both sides. It might be contained or not. If it is contained, then this
-            // contained row is settled water and we need to move the spring up one.
-            val row = checkRow(spring, settledWater, flowingWater)
-            val newSprings = otherSprings + row.newSprings +
-                    (if (row.contained) setOf(spring + Point(0, -1)) else emptySet())
-            waterTiles(newSprings,
-                    settledWater + row.newSettledWater,
-                    flowingWater - row.newSettledWater + row.newFlowingWater)
-        } else {
-            // Air is underneath. Move the spring point down one and mark this point as flowing water.
-            waterTiles(otherSprings + underneath, settledWater, flowingWater + spring)
-        }
-    }
-
-    private fun checkRow(spring: Point, settledWater: Set<Point>, flowingWater: Set<Point>): SpreadResult {
-        val left = checkDirection(spring, -1, settledWater, flowingWater)
-        val right = checkDirection(spring, 1, settledWater, flowingWater)
-
-        val newWater = (left.newWater + right.newWater + spring).toSet()
-        val contained = left.terminus.blocksFlow && right.terminus.blocksFlow
-        val newSprings = listOfNotNull(left.newSpring, right.newSpring).toSet()
-
-        return SpreadResult(contained,
-                if (contained) newWater else emptySet(),
-                if (contained) emptySet() else newWater,
-                newSprings)
-    }
-
-    private data class SpreadResult(val contained: Boolean,
-                                    val newSettledWater: Set<Point>,
-                                    val newFlowingWater: Set<Point>,
-                                    val newSprings: Set<Point>)
-
-    private fun checkDirection(spring: Point,
-                               delta: Int,
-                               settledWater: Set<Point>,
-                               flowingWater: Set<Point>): DirectionResult {
-        return checkDirection(listOf(spring), delta, settledWater, flowingWater)
-    }
-
-    private tailrec fun checkDirection(newWater: List<Point>,
-                                       delta: Int,
-                                       settledWater: Set<Point>,
-                                       flowingWater: Set<Point>): DirectionResult {
-        val last = newWater.last()
-        val next = last + Point(delta, 0)
-        val underneath = last + Point(0, 1)
-
-        fun isEmpty(point: Point) = !clay.contains(point) &&
-                !settledWater.contains(point) &&
-                !flowingWater.contains(point)
-
-        return when {
-            isEmpty(underneath) -> DirectionResult(newWater, DirectionTerminus.NEW_SPRING)
-            clay.contains(next) -> DirectionResult(newWater, DirectionTerminus.CLAY)
-            flowingWater.contains(next) -> DirectionResult(newWater, DirectionTerminus.FLOWING_WATER)
-            settledWater.contains(next) -> DirectionResult(newWater, DirectionTerminus.SETTLED_WATER)
-            else -> checkDirection(newWater + next, delta, settledWater, flowingWater)
-        }
-    }
-
-    private enum class DirectionTerminus(val blocksFlow: Boolean) {
-        CLAY(blocksFlow = true),
-        NEW_SPRING(blocksFlow = false),
-        SETTLED_WATER(blocksFlow = true),
-        FLOWING_WATER(blocksFlow = false)
-    }
-
-    private data class DirectionResult(val newWater: List<Point>,
-                                       val terminus: DirectionTerminus) {
-        val newSpring: Point? = if (terminus == DirectionTerminus.NEW_SPRING) newWater.last() else null
-    }
-
-    fun display(): String {
-        val waterTiles = waterTiles()
-        return (0..maxY).joinToString("\n") { y ->
-            ((minX - 1)..(maxX + 1)).joinToString("") { x ->
-                when {
-                    clay.contains(Point(x, y)) -> "#"
-                    waterTiles.settledWater.contains(Point(x, y)) -> "~"
-                    waterTiles.flowingWater.contains(Point(x, y)) -> "|"
-                    else -> "."
-                }
-            }
-        }
-    }
+    private fun flowResult() = FlowResult(clay, Point(500, 0))
 
     private class Row(y: Int, minX: Int, maxX: Int) {
         companion object {
@@ -175,13 +57,98 @@ class WaterReservoir private constructor(rows: List<Row>,
     }
 }
 
-private fun <T> Sequence<T>.takeWhileInclusive(pred: (T) -> Boolean): Sequence<T> {
-    var shouldContinue = true
-    return takeWhile {
-        val result = shouldContinue
-        shouldContinue = pred(it)
-        result
+private data class Point(val x: Int, val y: Int) {
+    operator fun plus(other: Point) = Point(this.x + other.x, this.y + other.y)
+}
+
+private class FlowResult constructor(clay: Set<Point>, spring: Point) {
+    private enum class PointState(val display: String, val causesPressure: Boolean = false) {
+        FLOWING_WATER("|"),
+        SETTLED_WATER("~", causesPressure = true),
+        CLAY("#", causesPressure = true),
+        EMPTY(".")
     }
+
+    private val points: MutableMap<Point, PointState> = clay.associate { it to PointState.CLAY }.toMutableMap()
+
+    private val minY = clay.map { it.y }.min() ?: 0
+    private val maxY = clay.map { it.y }.max() ?: 0
+    private val minX = clay.map { it.x }.min() ?: 0
+    private val maxX = clay.map { it.x }.max() ?: 0
+
+    init {
+        this[spring] = PointState.FLOWING_WATER
+        flow(spring)
+    }
+
+    private operator fun get(point: Point): PointState = points[point] ?: PointState.EMPTY
+    private operator fun set(point: Point, state: PointState) {
+        points[point] = state
+    }
+
+    private fun flow(flowPoint: Point) {
+        if (flowPoint.y > maxY) return
+
+        val underneath = flowPoint + Point(0, 1)
+
+        if (this[underneath] == PointState.EMPTY) {
+            this[underneath] = PointState.FLOWING_WATER
+            flow(underneath)
+        }
+
+        val left = flowPoint + Point(-1, 0)
+
+        if (this[underneath].causesPressure && this[left] == PointState.EMPTY) {
+            this[left] = PointState.FLOWING_WATER
+            flow(left)
+        }
+
+        val right = flowPoint + Point(1, 0)
+
+        if (this[underneath].causesPressure && this[right] == PointState.EMPTY) {
+            this[right] = PointState.FLOWING_WATER
+            flow(right)
+        }
+
+        if (hasWalls(flowPoint)) {
+            fillLevel(flowPoint)
+        }
+    }
+
+    private fun hasWalls(point: Point) = hasWall(point, left = true) && hasWall(point, left = false)
+
+    private fun hasWall(point: Point, left: Boolean) = horizontalFrom(point, left = left)
+            .map {
+                when (this[it]) {
+                    PointState.EMPTY -> false
+                    PointState.CLAY -> true
+                    else -> null
+                }
+            }
+            .filterNotNull()
+            .first()
+
+    private fun fillLevel(point: Point) {
+        fillSide(point, left = true)
+        fillSide(point, left = false)
+    }
+
+    private fun fillSide(point: Point, left: Boolean) = horizontalFrom(point, left)
+            .takeWhile { this[it] != PointState.CLAY }
+            .forEach { this[it] = PointState.SETTLED_WATER }
+
+    private fun horizontalFrom(point: Point, left: Boolean) =
+            generateSequence(point) { it + Point(if (left) -1 else 1, 0) }
+
+    override fun toString(): String {
+        return (0..maxY).joinToString("\n") { y ->
+            ((minX - 1)..(maxX + 1)).joinToString("") { x -> this[Point(x, y)].display }
+        }
+    }
+
+    fun waterCount() = this.points
+            .filter { it.key.y in minY..maxY }
+            .count { it.value == PointState.FLOWING_WATER || it.value == PointState.SETTLED_WATER }
 }
 
 fun main() {
@@ -190,9 +157,9 @@ fun main() {
 
     val reservoir = WaterReservoir.parse(input)
 
-    println(reservoir.toString())
+    println(reservoir.display())
 
-    //920 is too low
+    //27994 is too low
     //29138 is too high
     println(reservoir.waterTileCount())
 }
