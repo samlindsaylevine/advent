185ba8085709b9ad0f4dca8a2a56df56be7c6f37
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2019-12-14 09:38:06 -0600

    2019, Day 14, part 2: ChemicalNanofactory binary search for max fuel producable.

--- src/main/kotlin/advent/year2019/day14/ChemicalNanofactory.kt
+++ src/main/kotlin/advent/year2019/day14/ChemicalNanofactory.kt
@@ -14,16 +14,17 @@ class ChemicalNanofactory private constructor(private val reactionsByProduct: Ma
     /**
      * The amount of ore it costs to make the desired amount of the desired chemical.
      */
-    fun oreCost(amount: Long, chemical: Chemical) = oreCost(mapOf(chemical to amount))
+    fun oreCost(amount: Long, chemical: Chemical) = synthesize(mapOf(chemical to amount))["ORE"] ?: 0
 
     /**
-     * How much ore it takes to assemble the provided quantities of each chemical.
+     * Starting with an amount of needed quantities of some chemicals, run reactions until we have as much as we
+     * need of each chemical - except ORE which we are allowed to still need more of.
      *
      * @param neededQuantities Some of these may be negative if we currently have a surplus.
      */
-    private tailrec fun oreCost(neededQuantities: Map<Chemical, Long>): Long {
+    private tailrec fun synthesize(neededQuantities: Map<Chemical, Long>): Map<Chemical, Long> {
         if (neededQuantities.filter { it.key != "ORE" }.all { it.value <= 0 }) {
-            return neededQuantities["ORE"] ?: 0
+            return neededQuantities
         }
 
         val (chemical, amount) = neededQuantities.filter { it.key != "ORE" && it.value > 0 }.asSequence().first()
@@ -40,13 +41,59 @@ class ChemicalNanofactory private constructor(private val reactionsByProduct: Ma
             nextQuantities.merge(reagant, times * reagantAmount, Long::plus)
         }
 
-        return oreCost(nextQuantities)
+        return synthesize(nextQuantities)
     }
 
+    /**
+     * We start with an estimate of (1 trillion)/(cost to make 1 fuel), which we know is possible;
+     * and then do a binary-search type approach to find the smallest number that could not be synthesized.
+     */
     fun maxFuelFrom(oreAmount: Long): Long {
-        TODO()
+        val amountToMakeOneFuel = this.oreCost(1, "FUEL")
+        val definitelyPossible = oreAmount / amountToMakeOneFuel
+        val range = findRange(oreAmount, definitelyPossible)
+        return binarySearch(oreAmount, range)
+    }
+
+    /**
+     * Given the amount of ore we have, and and amount that is definitely possible, find an initial range where the
+     * lower bound is possible and the upper bound is impossible. (Keep doubling the possible amount until we find
+     * an impossible.)
+     */
+    private tailrec fun findRange(oreAmount: Long, definitePossible: Long): PossibleFuelRange {
+        val double = definitePossible * 2
+        return if (isPossible(oreAmount, double)) {
+            findRange(oreAmount, double)
+        } else {
+            PossibleFuelRange(possible = definitePossible, impossible = double)
+        }
+    }
+
+    /**
+     * Whether we can synthesize the desired fuel amount with the provided ore amount.
+     */
+    private fun isPossible(oreAmount: Long, fuelAmount: Long): Boolean {
+        val oreCost = this.oreCost(fuelAmount, "FUEL")
+        return oreCost <= oreAmount
+    }
+
+    /**
+     * Given an ore amount and a possible range, find the largest value in that range that is possible to produce,
+     * by testing the midpoint and recursing.
+     */
+    private tailrec fun binarySearch(oreAmount: Long, range: PossibleFuelRange): Long {
+        if (range.impossible == range.possible + 1) return range.possible
+
+        val midpoint = (range.impossible + range.possible) / 2
+        return if (isPossible(oreAmount, midpoint)) {
+            binarySearch(oreAmount, PossibleFuelRange(midpoint, range.impossible))
+        } else {
+            binarySearch(oreAmount, PossibleFuelRange(range.possible, midpoint))
+        }
     }
 }
+
+private data class PossibleFuelRange(val possible: Long, val impossible: Long)
 
 fun Long.ceilingDivide(other: Long) = if (this % other == 0L) (this / other) else (this / other + 1)
 
@@ -82,4 +129,5 @@ fun main() {
     val factory = ChemicalNanofactory.parse(input)
 
     println(factory.oreCost(1, "FUEL"))
+    println(factory.maxFuelFrom(1_000_000_000_000))
 }
