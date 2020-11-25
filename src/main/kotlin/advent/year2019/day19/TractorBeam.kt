518baf457e5a79ccaf2d27f16265b477c34fff5c
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2020-11-24 18:33:09 -0600

    Day 19, part 2: DroneSystem closestContainedSquare.

--- src/main/kotlin/advent/year2019/day19/TractorBeam.kt
+++ src/main/kotlin/advent/year2019/day19/TractorBeam.kt
@@ -5,7 +5,46 @@ import advent.year2019.day5.IntcodeComputer
 import advent.year2019.day5.asInput
 
 class DroneSystem(val program: List<Long>) {
-  fun isBeingPulled(x: Long, y: Long): Boolean {
+
+  // Cache our computations so that we never have to recalculate a particular space more than once.
+  private val cache = mutableMapOf<Pair<Long, Long>, Boolean>()
+
+  fun isBeingPulled(x: Long, y: Long) = cache.getOrPut(x to y) { calculateBeingPulled(x, y) }
+
+  fun countClosest(squareLength: Long) = (0L until squareLength).sumOf { x ->
+    (0L until squareLength).count { y -> this.isBeingPulled(x, y) }
+  }
+
+  fun closestContainedSquare(squareLength: Long): Pair<Long, Long> =
+          inOrderOfCloseness().first { squareIsContained(it.first, it.second, squareLength) }
+
+  private fun squareIsContained(upperLeftX: Long, upperleftY: Long, squareLength: Long) =
+  // Speed up optimization based on a guess about the nature of the tractor beam: check the 4 corners of the
+          // square first!
+          isBeingPulled(upperLeftX, upperleftY) &&
+                  isBeingPulled(upperLeftX + squareLength - 1, upperleftY) &&
+                  isBeingPulled(upperLeftX, upperleftY + squareLength - 1) &&
+                  isBeingPulled(upperLeftX + squareLength - 1, upperleftY + squareLength - 1) &&
+                  // OK, also check the entire interior of the square.
+                  (upperLeftX until upperLeftX + squareLength).all { x ->
+                    (upperleftY until upperleftY + squareLength).all { y -> isBeingPulled(x, y) }
+                  }
+
+  private fun inOrderOfCloseness(): Sequence<Pair<Long, Long>> = sequence {
+    var current = 0L to 0L
+
+    while (true) {
+      yield(current)
+      current = if (current.second == 0L) {
+        println("Distance ${current.first + 1}")
+        0L to current.first + 1
+      } else {
+        current.first + 1 to current.second - 1
+      }
+    }
+  }
+
+  private fun calculateBeingPulled(x: Long, y: Long): Boolean {
     require(x >= 0)
     require(y >= 0)
     val computer = IntcodeComputer()
@@ -23,9 +62,11 @@ fun main() {
 
   val droneSystem = DroneSystem(program)
 
-  val pointsAffected = (0L until 50L).sumOf { x ->
-    (0L until 50L).count { y -> droneSystem.isBeingPulled(x, y) }
-  }
+  println(droneSystem.countClosest(50))
 
-  println(pointsAffected)
+  // This takes about 8 minutes, so there's probably some way to speed it up... but CPU time is cheaper than developer
+  // time. Ship it!
+  val closestSquare = droneSystem.closestContainedSquare(100)
+
+  println(closestSquare.first * 10000 + closestSquare.second)
 }
