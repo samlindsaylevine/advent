c0bf19eb6780f86844010b9440767a44eac01712
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2020-11-27 17:25:24 -0600

    Day 22, part 2: Modular arithmetic....

--- src/main/kotlin/advent/year2019/day22/SpaceDeck.kt
+++ src/main/kotlin/advent/year2019/day22/SpaceDeck.kt
@@ -5,43 +5,122 @@ import java.io.File
 data class SpaceDeck(val cards: List<Int>) {
 
   constructor(numCards: Int = 10_007) : this((0 until numCards).toList())
+}
 
-  fun dealIntoNewStack() = SpaceDeck(this.cards.reversed())
+sealed class ShuffleInstruction {
+  abstract operator fun invoke(deck: SpaceDeck): SpaceDeck
 
-  fun cut(n: Int) = when {
-    n > 0 -> SpaceDeck(cards.drop(n) + cards.take(n))
-    n < 0 -> SpaceDeck(cards.takeLast(-n) + cards.dropLast(-n))
+  // If this instruction is executed on a deck, which original position ends up at position i?
+  abstract fun originalPosition(i: Long, numCards: Long): Long
+}
+
+object DealIntoNewStack : ShuffleInstruction() {
+  override operator fun invoke(deck: SpaceDeck) = SpaceDeck(deck.cards.reversed())
+
+  override fun originalPosition(i: Long, numCards: Long) = numCards - 1 - i
+}
+
+class Cut(val n: Int) : ShuffleInstruction() {
+  override operator fun invoke(deck: SpaceDeck) = when {
+    n > 0 -> SpaceDeck(deck.cards.drop(n) + deck.cards.take(n))
+    n < 0 -> SpaceDeck(deck.cards.takeLast(-n) + deck.cards.dropLast(-n))
     // n == 0
-    else -> this
+    else -> deck
   }
 
-  fun dealWithIncrement(n: Int): SpaceDeck {
-    val newList = MutableList(cards.size) { 0 }
-    cards.forEachIndexed { i, card -> newList[(i * n) % cards.size] = card }
-    return SpaceDeck(newList)
+  override fun originalPosition(i: Long, numCards: Long) = when {
+    n > 0 && (i < numCards - n) -> i + n
+    n > 0 && (i >= numCards - n) -> i - numCards + n
+    n < 0 && (i < -n) -> i + numCards + n
+    n < 0 && (i >= -n) -> i + n
+    // n == 0
+    else -> i
   }
 }
 
-private fun String.toShuffleInstruction(): (SpaceDeck) -> SpaceDeck = when {
-  this == "deal into new stack" -> SpaceDeck::dealIntoNewStack
-  this.startsWith("cut ") -> { deck: SpaceDeck ->
-    deck.cut(this.substringAfter("cut ").toInt())
+class DealWithIncrement(val n: Int) : ShuffleInstruction() {
+  override operator fun invoke(deck: SpaceDeck): SpaceDeck {
+    val newList = MutableList(deck.cards.size) { 0 }
+    deck.cards.forEachIndexed { i, card -> newList[(i * n) % deck.cards.size] = card }
+    return SpaceDeck(newList)
   }
-  this.startsWith("deal with increment ") -> { deck: SpaceDeck ->
-    deck.dealWithIncrement(this.substringAfter("deal with increment ").toInt())
-  }
+
+  /**
+   * Card x went to position (x * n) % numCards.
+   *
+   * So, we need to solve for x: (x * n) % numCards = i.
+   * This is the multiplicative inverse mod numCards!
+   */
+  override fun originalPosition(i: Long, numCards: Long) = (i * multiplicativeInverse(n.toLong(), numCards)) % numCards
+
+}
+
+/**
+ * This, in general, requires some use of the Euclidean algorithm...
+ * but here, we happen to know that the only numCards we use are _prime numbers_, i.e., 10007 and
+ * 119315717514047.
+ *
+ * Since x^(phi(m)) = 1 mod m; primality gives us phi(m) = m - 1; so our multiplicative inverse is x^(m-2) mod m.
+ */
+fun multiplicativeInverse(x: Long, m: Long): Long {
+  // We're cheating a little - the reference examples use 10 cards, which isn't prime, so we pre-calculate the phi
+  // function just for that value.
+  val phi = if (m == 10L) 4 else m - 1
+  return expMod(x, phi - 1, m)
+}
+
+/**
+ * Calculate x^n mod m.
+ */
+tailrec fun expMod(x: Long, n: Long, m: Long, current: Long = 1): Long = when {
+  n == 0L -> current
+  n % 2 == 0L -> expMod((x * x) % m, n / 2, m, current)
+  else -> expMod(x, n - 1, m, (x * current) % m)
+}
+
+private fun String.toShuffleInstruction(): ShuffleInstruction = when {
+  this == "deal into new stack" -> DealIntoNewStack
+  this.startsWith("cut ") -> Cut(this.substringAfter("cut ").toInt())
+  this.startsWith("deal with increment ") -> DealWithIncrement(this.substringAfter("deal with increment ").toInt())
   else -> throw IllegalArgumentException("Unrecognized instruction: $this")
 }
 
-fun List<String>.shuffle(startingDeck: SpaceDeck) = this.fold(startingDeck) { deck, instruction ->
-  instruction.toShuffleInstruction()(deck)
+fun List<String>.toInstructions() = this.map { it.toShuffleInstruction() }
+
+fun List<ShuffleInstruction>.shuffle(startingDeck: SpaceDeck) = this.fold(startingDeck) { deck, instruction ->
+  instruction(deck)
 }
 
+/**
+ * If this list of instructions is executed on a deck, which original position ends up in position i?
+ */
+fun List<ShuffleInstruction>.originalPosition(i: Long, numCards: Long) = this.reversed()
+        .fold(i) { position, instruction -> instruction.originalPosition(position, numCards) }
+
+/**
+ * Trying to see if we have a cycle in some small number of executions -- but we don't.
+ */
+fun List<ShuffleInstruction>.countToCycle(i: Long, numCards: Long) = this.countToCycle(originalPosition(i, numCards),
+        i,
+        numCards,
+        1)
+
+private tailrec fun List<ShuffleInstruction>.countToCycle(currentPos: Long,
+                                                          targetPos: Long,
+                                                          numCards: Long,
+                                                          numExecutions: Long): Long = when (targetPos) {
+  currentPos -> numExecutions
+  else -> countToCycle(this.originalPosition(currentPos, numCards), targetPos, numCards, numExecutions + 1)
+}
+
+fun puzzleInput() = File("src/main/kotlin/advent/year2019/day22/input.txt")
+        .readText()
+        .trim()
+        .lines()
+        .toInstructions()
+
 fun main() {
-  val input = File("src/main/kotlin/advent/year2019/day22/input.txt")
-          .readText()
-          .trim()
-          .lines()
+  val input = puzzleInput()
 
   val result = input.shuffle(SpaceDeck())
 
--- src/test/kotlin/advent/year2019/day22/SpaceDeckTest.kt
+++ src/test/kotlin/advent/year2019/day22/SpaceDeckTest.kt
@@ -2,33 +2,35 @@ package advent.year2019.day22
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class SpaceDeckTest {
 
   @Test
   fun `dealIntoNewStack -- reference example -- reverses`() {
-    val result = SpaceDeck(10).dealIntoNewStack()
+    val result = DealIntoNewStack(SpaceDeck(10))
 
     assertThat(result.cards).containsExactly(9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
   }
 
   @Test
   fun `cut -- reference example, 3 cards -- reference output`() {
-    val result = SpaceDeck(10).cut(3)
+    val result = Cut(3)(SpaceDeck(10))
 
     assertThat(result.cards).containsExactly(3, 4, 5, 6, 7, 8, 9, 0, 1, 2)
   }
 
   @Test
   fun `cut -- reference example, -4 cards -- reference output`() {
-    val result = SpaceDeck(10).cut(-4)
+    val result = Cut(-4)(SpaceDeck(10))
 
     assertThat(result.cards).containsExactly(6, 7, 8, 9, 0, 1, 2, 3, 4, 5)
   }
 
   @Test
   fun `dealWithIncrement -- reference example -- reference output`() {
-    val result = SpaceDeck(10).dealWithIncrement(3)
+    val result = DealWithIncrement(3)(SpaceDeck(10))
 
     assertThat(result.cards).containsExactly(0, 7, 4, 1, 8, 5, 2, 9, 6, 3)
   }
@@ -83,4 +85,107 @@ class SpaceDeckTest {
 
     assertThat(result.cards).containsExactly(9, 2, 5, 8, 1, 4, 7, 0, 3, 6)
   }
+
+  /**
+   * Don't have much of an example to unit test originalPosition of the whole instruction set with, except our answer
+   * for part 1!
+   */
+  @Test
+  fun `originalPosition -- puzzle input, part 1 answer -- 2019`() {
+    val result = puzzleInput().originalPosition(4485, numCards = 10007)
+
+    assertThat(result).isEqualTo(2019)
+  }
+
+  @ParameterizedTest(name = "originalPosition -- cut, position {0} -- started at {1}")
+  @CsvSource("0, 3",
+          "1, 4",
+          "2, 5",
+          "3, 6",
+          "4, 7",
+          "5, 8",
+          "6, 9",
+          "7, 0",
+          "8, 1",
+          "9, 2")
+  fun `originalPosition -- cut -- reference values`(finalPosition: Long, expected: Long) {
+    val result = Cut(3).originalPosition(finalPosition, numCards = 10)
+
+    assertThat(result).isEqualTo(expected)
+  }
+
+  @ParameterizedTest(name = "originalPosition -- negative cut, position {0} -- started at {1}")
+  @CsvSource("0, 6",
+          "1, 7",
+          "2, 8",
+          "3, 9",
+          "4, 0",
+          "5, 1",
+          "6, 2",
+          "7, 3",
+          "8, 4",
+          "9, 5")
+  fun `originalPosition -- negative cut -- reference values`(finalPosition: Long, expected: Long) {
+    val result = Cut(-4).originalPosition(finalPosition, numCards = 10)
+
+    assertThat(result).isEqualTo(expected)
+  }
+
+  @ParameterizedTest(name = "originalPosition -- deal into new stack, position {0} -- started at {1}")
+  @CsvSource("0, 9",
+          "1, 8",
+          "2, 7",
+          "3, 6",
+          "4, 5",
+          "5, 4",
+          "6, 3",
+          "7, 2",
+          "8, 1",
+          "9, 0")
+  fun `originalPosition -- deal into new stack -- reference values`(finalPosition: Long, expected: Long) {
+    val result = DealIntoNewStack.originalPosition(finalPosition, numCards = 10)
+
+    assertThat(result).isEqualTo(expected)
+  }
+
+  @ParameterizedTest(name = "originalPosition -- deal into new stack, position {0} -- started at {1}")
+  @CsvSource("0, 0",
+          "1, 7",
+          "2, 4",
+          "3, 1",
+          "4, 8",
+          "5, 5",
+          "6, 2",
+          "7, 9",
+          "8, 6",
+          "9, 3")
+  fun `originalPosition -- deal with increment -- reference values`(finalPosition: Long, expected: Long) {
+    val result = DealWithIncrement(3).originalPosition(finalPosition, numCards = 10)
+
+    assertThat(result).isEqualTo(expected)
+  }
+
+  @Test
+  fun `multiplicative inverse -- sample input -- really is the inverse`() {
+    val actual = multiplicativeInverse(53, 10007)
+
+    println(actual)
+
+    assertThat((actual * 53) % 10007).isEqualTo(1)
+  }
+
+  @ParameterizedTest(name = "exp mod -- {0}^{1} mod {2} -- {3}")
+  @CsvSource("2, 4, 100, 16",
+          "2, 4, 10, 6",
+          "2, 3, 100, 8",
+          "2, 3, 5, 3",
+          "3, 6, 1000, 729",
+          "3, 6, 10, 9")
+  fun `exp mod -- sample input -- gives expected output`(x: Long, n: Long, m: Long, expected: Long) {
+    val actual = expMod(x, n, m)
+
+    assertThat(actual).isEqualTo(expected)
+  }
+
+  private fun List<String>.shuffle(startingDeck: SpaceDeck) = this.toInstructions().shuffle(startingDeck)
 }
