217a8825feca51703b106b341bfa3e0dac050a8d
Sam Lindsay-Levine <sam.lindsay-levine@code42.com>
2020-11-27 18:39:32 -0600

    2019, Day 23, part 2: NAT.

--- src/main/kotlin/advent/year2019/day23/IntcodeNetwork.kt
+++ src/main/kotlin/advent/year2019/day23/IntcodeNetwork.kt
@@ -5,13 +5,13 @@ import advent.year2019.day5.IntcodeComputer
 import java.util.concurrent.*
 
 class IntcodeNetwork(val program: List<Long>) {
-  private var finalPacket = CompletableFuture<Packet>()
   private val computers = (0L..49L).associateWith { NetworkedComputer(it, program, this) }
+  private val nat = NotAlwaysTransferring(this)
 
   fun send(address: Long, packet: Packet) {
-    println("Sending $packet to $address")
+    // println("Sending $packet to $address")
     if (address == 255L) {
-      finalPacket.complete(packet)
+      nat.send(packet)
     } else {
       val computer = computers[address]
       if (computer == null) {
@@ -23,18 +23,24 @@ class IntcodeNetwork(val program: List<Long>) {
 
   }
 
-  fun run(): Long {
-    finalPacket = CompletableFuture()
+  fun isIdle() = computers.values.all { it.isIdle() }
 
-    val executor = Executors.newFixedThreadPool(computers.size) { runnable ->
+  fun run(until: NetworkMode): Long {
+    val executor = Executors.newFixedThreadPool(computers.size + 1) { runnable ->
       Thread(runnable).apply { isDaemon = true }
     }
+
+    executor.submit(nat::start)
 
     computers.values.forEach {
       executor.submit(it::start)
     }
 
-    val result = finalPacket.get()
+    val resultFuture = when (until) {
+      NetworkMode.FIRST_PACKET_TO_NAT -> nat.firstReceived
+      NetworkMode.FIRST_REPEATED_PACKET_FROM_NAT -> nat.firstSentTwice
+    }
+    val result = resultFuture.get()
 
     executor.shutdownNow()
 
@@ -45,11 +51,16 @@ class IntcodeNetwork(val program: List<Long>) {
 class NetworkedComputer(private val networkAddress: Long,
                         private val program: List<Long>,
                         private val network: IntcodeNetwork) {
-  private val queue: BlockingQueue<Packet> = LinkedBlockingQueue()
+  val queue: BlockingQueue<Packet> = LinkedBlockingQueue()
 
   private var everGotNetworkAddress = false
   private var partialInput: Long? = null
   private val partialOutput = mutableListOf<Long>()
+
+  @Volatile
+  var seekingInput: Boolean = false
+
+  fun isIdle() = queue.isEmpty() && seekingInput
 
   fun send(packet: Packet) = queue.put(packet)
 
@@ -71,8 +82,14 @@ class NetworkedComputer(private val networkAddress: Long,
       return existing
     }
 
-    val fromQueue = queue.poll() ?: return -1
+    val fromQueue = queue.poll()
 
+    if (fromQueue == null) {
+      seekingInput = true
+      return -1
+    }
+
+    seekingInput = false
     partialInput = fromQueue.y
     return fromQueue.x
   }
@@ -87,14 +104,55 @@ class NetworkedComputer(private val networkAddress: Long,
   }
 }
 
+
 data class Packet(val x: Long, val y: Long)
+
+class NotAlwaysTransferring(val network: IntcodeNetwork) {
+  val firstReceived = CompletableFuture<Packet>()
+
+  private var lastReceived: Packet? = null
+  private var lastSent: Packet? = null
+
+  val firstSentTwice = CompletableFuture<Packet>()
+
+  private val queue: BlockingQueue<Packet> = LinkedBlockingQueue()
+
+  fun send(packet: Packet) = queue.put(packet)
+
+  fun start() {
+    // println("NAT starting")
+    while (true) {
+      val packet = queue.poll()
+      if (packet != null) {
+        // println("NAT received $packet")
+        if (!firstReceived.isDone) firstReceived.complete(packet)
+        lastReceived = packet
+      } else if (network.isIdle() && queue.isEmpty()) {
+        val toSend = lastReceived
+        if (toSend == null) {
+          // println("Network is idle but NAT never got a packet")
+        } else {
+          if (lastSent == toSend) firstSentTwice.complete(toSend)
+          lastSent = toSend
+          println("NAT sending $toSend")
+          network.send(0, toSend)
+        }
+      }
+    }
+  }
+}
+
+enum class NetworkMode {
+  FIRST_PACKET_TO_NAT,
+  FIRST_REPEATED_PACKET_FROM_NAT
+}
 
 fun main() {
   val program = parseIntcodeFromFile("src/main/kotlin/advent/year2019/day23/input.txt")
 
-  val network = IntcodeNetwork(program)
+  val part1 = IntcodeNetwork(program).run(NetworkMode.FIRST_PACKET_TO_NAT)
+  println("Part 1: $part1")
 
-  val result = network.run()
-
-  println(result)
+  val part2 = IntcodeNetwork(program).run(NetworkMode.FIRST_REPEATED_PACKET_FROM_NAT)
+  println("Part 2: $part2")
 }
