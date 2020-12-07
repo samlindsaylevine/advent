88c89a9c7534fa9fc1f2ce6bd3cb0a9a62fd613f
Sam Lindsay-Levine <slindsaylevine+github@gmail.com>
2020-12-07 09:33:07 -0600

    2020, Day 7: BagRules.

---
+++ src/main/kotlin/advent/year2020/day7/BagRules.kt
@@ -0,0 +1,68 @@
+package advent.year2020.day7
+
+import com.google.common.cache.CacheBuilder
+import com.google.common.cache.CacheLoader
+import java.io.File
+
+class BagRules(val rules: List<BagRule>) {
+  constructor(input: String) : this(input.trim().lines().map(BagRule::parse))
+
+  // Cache the contained-inside counts so we don't have to recalculate for any particular type of bag.
+  private val containedInsideCache = CacheBuilder.newBuilder()
+          .build(CacheLoader.from { bag: String? -> calculateContainedInside(bag!!) })
+
+  fun validOutmost(inner: String): Set<String> = validOutmost(setOf(inner), emptySet())
+
+  private tailrec fun validOutmost(inner: Set<String>, alreadyValid: Set<String>): Set<String> {
+    if (inner.isEmpty()) return alreadyValid
+
+    val nextCandidates = inner.flatMap { bag ->
+      rules.filter { it.inner.containsKey(bag) }.map { it.outer }
+    }.filter { it !in alreadyValid }
+            .toSet()
+
+    return validOutmost(nextCandidates, alreadyValid + nextCandidates)
+  }
+
+  fun containedInside(outermost: String): Int = containedInsideCache.get(outermost)
+
+  private fun calculateContainedInside(outermost: String) =
+          rules.first { it.outer == outermost }
+                  .inner
+                  .entries
+                  // 1 for the bag itself, plus everything inside that one
+                  .sumOf { it.value * (1 + containedInside(it.key)) }
+}
+
+data class BagRule(val outer: String, val inner: Map<String, Int>) {
+  companion object {
+    private val REGEX = "(.+) bags contain (.+).".toRegex()
+    private val INNER_REGEX = "(\\d+) (.+) bags?".toRegex()
+
+    fun parse(input: String): BagRule {
+      val match = REGEX.matchEntire(input) ?: throw IllegalArgumentException("Unparseable rule $input")
+
+      val outer = match.groupValues[1]
+      val inner = when (val inside = match.groupValues[2]) {
+        "no other bags" -> emptyMap()
+        else -> inside.split(", ").map(::parseInner).toMap()
+      }
+      return BagRule(outer, inner)
+    }
+
+    private fun parseInner(inner: String): Pair<String, Int> {
+      val match = INNER_REGEX.matchEntire(inner) ?: throw IllegalArgumentException("Unparseable inner $inner")
+      return match.groupValues[2] to match.groupValues[1].toInt()
+    }
+  }
+}
+
+fun main() {
+  val input = File("src/main/kotlin/advent/year2020/day7/input.txt")
+          .readText()
+
+  val rules = BagRules(input)
+
+  println(rules.validOutmost("shiny gold").size)
+  println(rules.containedInside("shiny gold"))
+}
---
+++ src/test/kotlin/advent/year2020/day7/BagRulesTest.kt
@@ -0,0 +1,67 @@
+package advent.year2020.day7
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class BagRulesTest {
+
+  @Test
+  fun `validOutermost -- reference example -- reference values`() {
+    val rules = BagRules("""
+      light red bags contain 1 bright white bag, 2 muted yellow bags.
+      dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+      bright white bags contain 1 shiny gold bag.
+      muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+      shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+      dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+      vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+      faded blue bags contain no other bags.
+      dotted black bags contain no other bags.
+    """.trimIndent())
+
+    val outermost = rules.validOutmost("shiny gold")
+
+    assertThat(outermost).containsExactlyInAnyOrder(
+            "bright white",
+            "muted yellow",
+            "dark orange",
+            "light red"
+    )
+  }
+
+  @Test
+  fun `containedInside -- first reference example, shiny gold -- 32 bags`() {
+    val rules = BagRules("""
+      light red bags contain 1 bright white bag, 2 muted yellow bags.
+      dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+      bright white bags contain 1 shiny gold bag.
+      muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+      shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+      dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+      vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+      faded blue bags contain no other bags.
+      dotted black bags contain no other bags.
+    """.trimIndent())
+
+    val count = rules.containedInside("shiny gold")
+
+    assertThat(count).isEqualTo(32)
+  }
+
+  @Test
+  fun `containedInside -- second reference example, shiny gold -- 126 bags`() {
+    val rules = BagRules("""
+      shiny gold bags contain 2 dark red bags.
+      dark red bags contain 2 dark orange bags.
+      dark orange bags contain 2 dark yellow bags.
+      dark yellow bags contain 2 dark green bags.
+      dark green bags contain 2 dark blue bags.
+      dark blue bags contain 2 dark violet bags.
+      dark violet bags contain no other bags.
+    """.trimIndent())
+
+    val count = rules.containedInside("shiny gold")
+
+    assertThat(count).isEqualTo(126)
+  }
+}
