4fbee922a38a220a0ca73a3175f022510552b1f3
Sam Lindsay-Levine <slindsaylevine+github@gmail.com>
2020-12-16 22:28:21 -0600

    2020, day 16, part 2: TrainTicket correct rule ordering.

--- src/main/kotlin/advent/year2020/day16/TrainTicket.kt
+++ src/main/kotlin/advent/year2020/day16/TrainTicket.kt
@@ -6,6 +6,8 @@ class TrainTicket(val values: List<Int>) {
   constructor(input: String) : this(input.split(",").map { it.toInt() })
 
   fun invalidValues(fields: List<TrainTicketField>) = values.filter { value -> fields.none { it.isValid(value) } }
+
+  fun passes(rule: TrainTicketField, index: Int) = rule.isValid(values[index])
 }
 
 class TrainTicketField(val name: String, val ranges: List<IntRange>) {
@@ -19,9 +21,9 @@ class TrainTicketField(val name: String, val ranges: List<IntRange>) {
   fun isValid(value: Int) = ranges.any { it.contains(value) }
 }
 
-class TrainTicketDocument(val rules: List<TrainTicketField>,
-                          val myTicket: TrainTicket,
-                          val nearbyTickets: List<TrainTicket>) {
+class TrainTicketDocument(private val rules: List<TrainTicketField>,
+                          private val myTicket: TrainTicket,
+                          private val nearbyTickets: List<TrainTicket>) {
   companion object {
     fun parse(input: String): TrainTicketDocument {
       val sections = input.split("\n\n")
@@ -35,6 +37,53 @@ class TrainTicketDocument(val rules: List<TrainTicketField>,
 
   fun errorRate() = nearbyTickets.flatMap { it.invalidValues(rules) }
           .sum()
+
+  val validNearbyTickets by lazy {
+    nearbyTickets.filter { it.invalidValues(rules).isEmpty() }
+  }
+
+  private val ruleValidityCache = mutableMapOf<Int, List<TrainTicketField>>()
+
+  private fun validRulesAtPosition(position: Int) = ruleValidityCache.getOrPut(position) {
+    rules.filter { rule -> validNearbyTickets.all { it.passes(rule, position) } }
+  }
+
+  val correctRuleOrdering: List<TrainTicketField> by lazy {
+    findOrdering(emptyMap(), rules.toSet()) ?: throw IllegalStateException("No legal arrangement of rules")
+  }
+
+  private fun findOrdering(partialOrdering: Map<Int, TrainTicketField>,
+                           remainingRules: Set<TrainTicketField>): List<TrainTicketField>? {
+    if (remainingRules.isEmpty()) return rules.indices
+            .map { partialOrdering[it] ?: throw IllegalStateException("Missing rule $it") }
+
+    val possibilities = rules.indices
+            .filter { it !in partialOrdering }
+            .associateWith { (validRulesAtPosition(it) intersect remainingRules) }
+
+    // As a speed up optimization, consider as our next index the one with the fewest options.
+    val next = possibilities.minByOrNull { it.value.size }
+            ?: return null
+
+    return next.value.asSequence()
+            .map { possibleNext ->
+              findOrdering(partialOrdering + (next.key to possibleNext),
+                      remainingRules - possibleNext)
+            }
+            .filterNotNull()
+            .firstOrNull()
+  }
+
+  val myTicketAsMap by lazy {
+    correctRuleOrdering.mapIndexed { index, rule -> rule.name to myTicket.values[index] }
+            .toMap()
+  }
+
+  val productOfDepartureFields by lazy {
+    myTicketAsMap.entries.filter { it.key.startsWith("departure") }
+            .map { it.value.toLong() }
+            .reduce(Long::times)
+  }
 }
 
 private fun String.toIntRange(delimiter: String = "-") =
@@ -48,4 +97,5 @@ fun main() {
   val document = TrainTicketDocument.parse(input)
 
   println(document.errorRate())
+  println(document.productOfDepartureFields)
 }
--- src/test/kotlin/advent/year2020/day16/TrainTicketTest.kt
+++ src/test/kotlin/advent/year2020/day16/TrainTicketTest.kt
@@ -26,4 +26,29 @@ class TrainTicketTest {
 
     assertThat(errorRate).isEqualTo(71)
   }
+
+  @Test
+  fun `myTicketAsMap -- reference input -- class 12, row 11, seat 13`() {
+    val document = TrainTicketDocument.parse("""
+      class: 0-1 or 4-19
+      row: 0-5 or 8-19
+      seat: 0-13 or 16-19
+
+      your ticket:
+      11,12,13
+
+      nearby tickets:
+      3,9,18
+      15,1,5
+      5,14,9
+    """.trimIndent())
+
+    val decodedTicket = document.myTicketAsMap
+
+    assertThat(decodedTicket).isEqualTo(mapOf(
+            "class" to 12,
+            "row" to 11,
+            "seat" to 13
+    ))
+  }
 }
