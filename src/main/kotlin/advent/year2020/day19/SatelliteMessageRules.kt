5ffc914ecc52970fd1b207ffadf706adeb50ba80
Sam Lindsay-Levine <slindsaylevine+github@gmail.com>
2020-12-19 11:13:19 -0600

    2020, day 19, part 2: Recursive SatelliteMessageRules. "Any number of time" basically means "up to 4 times", am I right?

--- src/main/kotlin/advent/year2020/day19/SatelliteMessageRules.kt
+++ src/main/kotlin/advent/year2020/day19/SatelliteMessageRules.kt
@@ -14,6 +14,43 @@ class SatelliteMessageRules(input: String) {
     val rule = rules[ruleNumber] ?: throw IllegalArgumentException("No rule $ruleNumber")
     rule.regex(this)
   }
+
+  fun regexWithRecursion(): Regex {
+    // This is tuned to match our input only (and also the example). It won't work for the general case.
+    requireRule(0, SingleGroupRule(listOf(8, 11)))
+    requireRule(8, SingleGroupRule(listOf(42)))
+    requireRule(11, SingleGroupRule(listOf(42, 31)))
+
+    val fortyTwo = regexString(42)
+
+    // This one is actually expressible as a regex - it's "forty-two+"; one or more copies of rule forty-two.
+    val eightRegex = "$fortyTwo+"
+
+    // This one is not expressible as a regex.
+    //
+    // It's "42 31" or "42 42 31 31" or ... "42^n 31^n".
+    //
+    // a^n b^n cannot be matched by a regular expression because it can't be
+    // captured in a finite state automaton - you have to count an unbounded number of a's.
+    // So, we're going to cheat a bit - we're going to just include up to 4 copies!
+    //
+    // This is, again, good enough for our specific input... if it wasn't, I would include more finite copies :B
+
+    val thirtyOne = regexString(31)
+
+    val elevenRegex = "(?:$fortyTwo$thirtyOne|" +
+            "$fortyTwo$fortyTwo$thirtyOne$thirtyOne|" +
+            "$fortyTwo$fortyTwo$fortyTwo$thirtyOne$thirtyOne$thirtyOne|" +
+            "$fortyTwo$fortyTwo$fortyTwo$fortyTwo$thirtyOne$thirtyOne$thirtyOne$thirtyOne)"
+
+    return (eightRegex + elevenRegex).toRegex()
+  }
+
+  private fun requireRule(number: Int, expectedRule: SatelliteMessageRule) {
+    if (rules[number] != expectedRule) {
+      throw IllegalStateException("Expected rule $number to be $expectedRule but was ${rules[number]}")
+    }
+  }
 }
 
 private sealed class SatelliteMessageRule {
@@ -41,7 +78,7 @@ private class LiteralRule(val literal: String) : SatelliteMessageRule() {
   override fun regex(rules: SatelliteMessageRules) = literal
 }
 
-private class SingleGroupRule(val ruleNumbers: List<Int>) : SatelliteMessageRule() {
+private data class SingleGroupRule(val ruleNumbers: List<Int>) : SatelliteMessageRule() {
   override fun regex(rules: SatelliteMessageRules) = ruleNumbers.joinToString(separator = "") { rules.regexString(it) }
 }
 
@@ -63,4 +100,5 @@ fun main() {
   val ruleZero = rules.regexString(0).toRegex()
 
   println(messages.count { ruleZero.matches(it) })
+  println(messages.count { rules.regexWithRecursion().matches(it) })
 }
