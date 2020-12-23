90a09cca488dbccd8223085f2fa82ae43e6dc8a0
Sam Lindsay-Levine <slindsaylevine+github@gmail.com>
2020-12-22 20:45:43 -0600

    2020, day 22, part 2: Recursive CombatCardGame.

--- src/main/kotlin/advent/year2020/day22/CombatCardGame.kt
+++ src/main/kotlin/advent/year2020/day22/CombatCardGame.kt
@@ -4,8 +4,8 @@ import java.io.File
 import java.util.*
 
 
-class CombatCardGame(val playerOne: Deque<Int>,
-                     val playerTwo: Deque<Int>) {
+class CombatCardGame(private val playerOneDeck: List<Int>,
+                     private val playerTwoDeck: List<Int>) {
 
   companion object {
     fun parse(input: String): CombatCardGame {
@@ -22,26 +22,61 @@ class CombatCardGame(val playerOne: Deque<Int>,
   /**
    * Plays the game until one player's deck is empty and returns their score.
    */
-  fun play(): Int {
+  fun play(recursive: Boolean = false): GameResult {
+    val seenStates = mutableSetOf<Pair<List<Int>, List<Int>>>()
+
+    // Make mutable copies so we don't mess up the original.
+    val playerOne = LinkedList(playerOneDeck)
+    val playerTwo = LinkedList(playerTwoDeck)
+
     while (playerOne.isNotEmpty() && playerTwo.isNotEmpty()) {
+
+      val currentState = playerOne.toList() to playerTwo.toList()
+      if (seenStates.contains(currentState)) {
+        return GameResult(winner = Winner.PLAYER_1, playerOne.score())
+      }
+      seenStates.add(currentState)
+
       val playerOneCard = playerOne.pop()
       val playerTwoCard = playerTwo.pop()
 
-      if (playerOneCard > playerTwoCard) {
-        playerOne.addLast(playerOneCard)
-        playerOne.addLast(playerTwoCard)
-      } else {
-        playerTwo.addLast(playerTwoCard)
-        playerTwo.addLast(playerOneCard)
+      val roundWinner = when {
+        recursive && playerOne.size >= playerOneCard && playerTwo.size >= playerTwoCard -> {
+          val subgame = CombatCardGame(LinkedList(playerOne.take(playerOneCard)),
+                  LinkedList(playerTwo.take(playerTwoCard)))
+          subgame.play(recursive = true).winner
+        }
+        playerOneCard > playerTwoCard -> Winner.PLAYER_1
+        else -> Winner.PLAYER_2
+      }
+
+      when (roundWinner) {
+        Winner.PLAYER_1 -> {
+          playerOne.addLast(playerOneCard)
+          playerOne.addLast(playerTwoCard)
+        }
+        Winner.PLAYER_2 -> {
+          playerTwo.addLast(playerTwoCard)
+          playerTwo.addLast(playerOneCard)
+        }
       }
     }
 
-    val winningPlayer = if (playerOne.isEmpty()) playerTwo else playerOne
+    val winningDeck = if (playerOne.isEmpty()) playerTwo else playerOne
 
-    return winningPlayer.mapIndexed { i, num -> (winningPlayer.size - i) * num }
-            .sum()
+    return GameResult(if (playerOne.isEmpty()) Winner.PLAYER_2 else Winner.PLAYER_1,
+            winningDeck.score())
   }
+
+  private fun Deque<Int>.score() = this.mapIndexed { i, num -> (this.size - i) * num }
+          .sum()
 }
+
+enum class Winner {
+  PLAYER_1, PLAYER_2
+}
+
+data class GameResult(val winner: Winner, val score: Int)
 
 fun main() {
   val input = File("src/main/kotlin/advent/year2020/day22/input.txt")
@@ -50,4 +85,5 @@ fun main() {
 
   val game = CombatCardGame.parse(input)
   println(game.play())
+  println(game.play(recursive = true))
 }
--- src/test/kotlin/advent/year2020/day22/CombatCardGameTest.kt
+++ src/test/kotlin/advent/year2020/day22/CombatCardGameTest.kt
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.Test
 
 class CombatCardGameTest {
 
-  @Test
-  fun `play -- reference input -- score 306`() {
-    val input = """
+  private val input = """
       Player 1:
       9
       2
@@ -23,10 +21,41 @@ class CombatCardGameTest {
       10
     """.trimIndent()
 
+  @Test
+  fun `play -- reference input -- score 306`() {
     val game = CombatCardGame.parse(input)
 
-    val score = game.play()
+    val score = game.play().score
 
     assertThat(score).isEqualTo(306)
+  }
+
+  @Test
+  fun `play -- infinite loop -- player 1 wins`() {
+    val input = """
+      Player 1:
+      43
+      19
+
+      Player 2:
+      2
+      29
+      14
+    """.trimIndent()
+
+    val game = CombatCardGame.parse(input)
+
+    val result = game.play(recursive = true)
+
+    assertThat(result.winner).isEqualTo(Winner.PLAYER_1)
+  }
+
+  @Test
+  fun `play -- reference input, recursive -- score 291`() {
+    val game = CombatCardGame.parse(input)
+
+    val score = game.play(recursive = true).score
+
+    assertThat(score).isEqualTo(291)
   }
 }
