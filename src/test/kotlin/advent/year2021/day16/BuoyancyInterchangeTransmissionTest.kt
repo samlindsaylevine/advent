1a227ff23181a7dd61918107685a5628ed8379f2
Sam Lindsay-Levine <slindsaylevine+github@gmail.com>
2021-12-16 10:10:22 -0500

    2021, Day 16: Packet evaluation.

--- src/test/kotlin/advent/year2021/day16/BuoyancyInterchangeTransmissionTest.kt
+++ src/test/kotlin/advent/year2021/day16/BuoyancyInterchangeTransmissionTest.kt
@@ -59,4 +59,21 @@ class BuoyancyInterchangeTransmissionTest {
 
     assertThat(packet.versionSum()).isEqualTo(expected)
   }
+
+  @ParameterizedTest
+  @CsvSource(
+    "C200B40A82, 3",
+    "04005AC33890, 54",
+    "880086C3E88112, 7",
+    "CE00C43D881120, 9",
+    "D8005AC2A8F0, 1",
+    "F600BC2D8F, 0",
+    "9C005AC2F8F0, 0",
+    "9C0141080250320F1802104A08, 1"
+  )
+  fun `evaluate -- reference examples -- reference values`(input: String, expected: Long) {
+    val packet = BuoyancyInterchangeTransmission.parse(input)
+
+    assertThat(packet.evaluate()).isEqualTo(expected)
+  }
 }
