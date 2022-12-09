e7a37d9f16a682526b688c138c95d02f8110604e
Sam Lindsay-Levine <slindsaylevine+github@gmail.com>
2022-12-09 08:51:23 -0600

    2022, Day 9, part 2: RopePath of arbitrary length.

--- src/test/kotlin/advent/year2022/day9/RopePathTest.kt
+++ src/test/kotlin/advent/year2022/day9/RopePathTest.kt
@@ -17,10 +17,48 @@ class RopePathTest {
       L 5
       R 2
     """.trimIndent()
-    val path = RopePath().move(input.lines())
+    val path = RopePath(2).move(input.lines())
 
     val visited = path.tailVisited
 
     assertThat(visited).hasSize(13)
+  }
+
+  @Test
+  fun `tail visited -- large rope, small reference input -- 1 position`() {
+    val input = """
+      R 4
+      U 4
+      L 3
+      D 1
+      R 4
+      D 1
+      L 5
+      R 2
+    """.trimIndent()
+    val path = RopePath(10).move(input.lines())
+
+    val visited = path.tailVisited
+
+    assertThat(visited).hasSize(1)
+  }
+
+  @Test
+  fun `tail visited -- large rope, large reference input -- 36 positions`() {
+    val input = """
+      R 5
+      U 8
+      L 8
+      D 3
+      R 17
+      D 10
+      L 25
+      U 20
+    """.trimIndent()
+    val path = RopePath(10).move(input.lines())
+
+    val visited = path.tailVisited
+
+    assertThat(visited).hasSize(36)
   }
 }
