056dfa9d3ee0f1e6a5e5ea48579ac2951f2c71fe
Sam Lindsay-Levine <slindsaylevine+github@gmail.com>
2023-12-02 08:48:00 -0600

    2023, day 2: Colored cube games.

---
+++ src/test/kotlin/advent/year2023/day2/ColoredCubeGamesTest.kt
@@ -0,0 +1,39 @@
+package advent.year2023.day2
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ColoredCubeGamesTest {
+
+    @Test
+    fun `possibleIdSum -- reference input -- 8`() {
+        val input = """
+            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """.trimIndent()
+
+        val games = ColoredCubeGames(input)
+        val sum = games.possibleIdSum(ColoredCubeGames.partOneColors)
+
+        assertThat(sum).isEqualTo(8)
+    }
+
+    @Test
+    fun `power -- reference input -- 2286`() {
+        val input = """
+            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """.trimIndent()
+
+        val games = ColoredCubeGames(input)
+        val power = games.totalPower()
+
+        assertThat(power).isEqualTo(2286)
+    }
+}
