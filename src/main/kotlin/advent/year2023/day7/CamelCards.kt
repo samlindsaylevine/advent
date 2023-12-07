8deb91122200b348726c85893de5c666b41723a8
Sam Lindsay-Levine <slindsaylevine+github@gmail.com>
2023-12-07 10:34:20 -0600

    2023, day 7: CamelCards.

---
+++ src/main/kotlin/advent/year2023/day7/CamelCards.kt
@@ -0,0 +1,115 @@
+package advent.year2023.day7
+
+import java.io.File
+
+class CamelCards(private val handsAndBids: List<HandAndBid>) {
+    constructor(input: String) : this(input.trim()
+            .lines()
+            .map {
+                val (handPart, bidPart) = it.split(" ")
+                HandAndBid(CamelCardHand(handPart), bidPart.toInt())
+            }
+    )
+
+    fun totalWinnings(comparator: Comparator<CamelCardHand> = CamelCardHand.normalRulesComparator): Int {
+        val sorted = handsAndBids.sortedWith(Comparator.comparing(HandAndBid::hand, comparator)).withIndex()
+        return sorted.sumOf { it.value.bid * (it.index + 1) }
+    }
+}
+
+data class HandAndBid(val hand: CamelCardHand, val bid: Int)
+
+enum class CamelCard(val char: Char) {
+    A('A'),
+    K('K'),
+    Q('Q'),
+    J('J'),
+    T('T'),
+    NINE('9'),
+    EIGHT('8'),
+    SEVEN('7'),
+    SIX('6'),
+    FIVE('5'),
+    FOUR('4'),
+    THREE('3'),
+    TWO('2');
+
+    companion object {
+        private val byChar = values().associateBy { it.char }
+        fun of(char: Char): CamelCard = byChar[char]
+                ?: throw IllegalArgumentException("Unrecognized card $char")
+
+        val jokersLowComparator: Comparator<CamelCard> = Comparator { left, right ->
+            when {
+                left == right -> 0
+                left == J -> 1
+                right == J -> -1
+                else -> left.compareTo(right)
+            }
+        }
+    }
+}
+
+enum class CamelCardHandType(val distinctCounts: List<Int>) {
+    FIVE_OF_A_KIND(5),
+    FOUR_OF_A_KIND(4, 1),
+    FULL_HOUSE(3, 2),
+    THREE_OF_A_KIND(3, 1, 1),
+    TWO_PAIR(2, 2, 1),
+    ONE_PAIR(2, 1, 1, 1),
+    HIGH_CARD(1, 1, 1, 1, 1);
+
+    companion object {
+        private val byCounts = values().associateBy { it.distinctCounts }
+        fun of(distinctCounts: List<Int>): CamelCardHandType = byCounts[distinctCounts]
+                ?: throw IllegalArgumentException("Unrecognized hand counts $distinctCounts")
+    }
+
+    constructor(vararg counts: Int) : this(counts.toList())
+}
+
+data class CamelCardHand(val cards: List<CamelCard>) : Comparable<CamelCardHand> {
+    constructor(input: String) : this(input.map(CamelCard::of))
+
+    companion object {
+        val normalRulesComparator: Comparator<CamelCardHand> = Comparator.comparing(CamelCardHand::type)
+                .thenComparing<CamelCard> { it.cards[0] }
+                .thenComparing<CamelCard> { it.cards[1] }
+                .thenComparing<CamelCard> { it.cards[2] }
+                .thenComparing<CamelCard> { it.cards[3] }
+                .thenComparing<CamelCard> { it.cards[4] }
+                .reversed()
+
+        val jokersWildComparator: Comparator<CamelCardHand> = Comparator.comparing(CamelCardHand::typeWithJokers)
+                .thenComparing({ it.cards[0] }, CamelCard.jokersLowComparator)
+                .thenComparing({ it.cards[1] }, CamelCard.jokersLowComparator)
+                .thenComparing({ it.cards[2] }, CamelCard.jokersLowComparator)
+                .thenComparing({ it.cards[3] }, CamelCard.jokersLowComparator)
+                .thenComparing({ it.cards[4] }, CamelCard.jokersLowComparator)
+                .reversed()
+    }
+
+    private fun List<CamelCard>.distinctCounts(): List<Int> = this.groupingBy { it }.eachCount().values.sortedDescending()
+    fun type(): CamelCardHandType = CamelCardHandType.of(cards.distinctCounts())
+
+    fun typeWithJokers(): CamelCardHandType {
+        val (jokers, withoutJokers) = cards.partition { it == CamelCard.J }
+        val countsWithoutJokers = withoutJokers.distinctCounts()
+        val updatedCounts = if (countsWithoutJokers.isEmpty()) {
+            listOf(jokers.size)
+        } else {
+            listOf(countsWithoutJokers.first() + jokers.size) + countsWithoutJokers.drop(1)
+        }
+        return CamelCardHandType.of(updatedCounts)
+    }
+
+    override fun compareTo(other: CamelCardHand): Int = normalRulesComparator.compare(this, other)
+}
+
+fun main() {
+    val input = File("src/main/kotlin/advent/year2023/day7/input.txt").readText().trim()
+    val cards = CamelCards(input)
+
+    println(cards.totalWinnings())
+    println(cards.totalWinnings(CamelCardHand.jokersWildComparator))
+}
---
+++ src/test/kotlin/advent/year2023/day7/CamelCardsTest.kt
@@ -0,0 +1,75 @@
+package advent.year2023.day7
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class CamelCardsTest {
+
+    @Test
+    fun `total winnings -- reference input -- 6440`() {
+        val input = """
+            32T3K 765
+            T55J5 684
+            KK677 28
+            KTJJT 220
+            QQQJA 483
+        """.trimIndent()
+
+        val cards = CamelCards(input)
+        val winnings = cards.totalWinnings()
+
+        assertThat(winnings).isEqualTo(6440)
+    }
+
+    @Test
+    fun `total winnings -- jokers wild -- 5905`() {
+        val input = """
+            32T3K 765
+            T55J5 684
+            KK677 28
+            KTJJT 220
+            QQQJA 483
+        """.trimIndent()
+
+        val cards = CamelCards(input)
+        val winnings = cards.totalWinnings(CamelCardHand.jokersWildComparator)
+
+        assertThat(winnings).isEqualTo(5905)
+    }
+
+    @ParameterizedTest(name = "hand types -- jokers wild, hand {0} -- {1}")
+    @CsvSource(
+            "32T3K, ONE_PAIR",
+            "KK677, TWO_PAIR",
+            "T55J5, FOUR_OF_A_KIND",
+            "KTJJT, FOUR_OF_A_KIND",
+            "QQQJA, FOUR_OF_A_KIND"
+    )
+    fun `hand types -- jokers wild -- hand type is as given`(handString: String, expectedHandType: CamelCardHandType) {
+        val hand = CamelCardHand(handString)
+        val type = hand.typeWithJokers()
+
+        assertThat(type).isEqualTo(expectedHandType)
+    }
+
+    @Test
+    fun `card orders -- sorted jokers low -- AKQ1098765432J`() {
+        val cards = CamelCard.values().sortedWith(CamelCard.jokersLowComparator)
+
+        assertThat(cards).containsExactly(CamelCard.A,
+                CamelCard.K,
+                CamelCard.Q,
+                CamelCard.T,
+                CamelCard.NINE,
+                CamelCard.EIGHT,
+                CamelCard.SEVEN,
+                CamelCard.SIX,
+                CamelCard.FIVE,
+                CamelCard.FOUR,
+                CamelCard.THREE,
+                CamelCard.TWO,
+                CamelCard.J)
+    }
+}
