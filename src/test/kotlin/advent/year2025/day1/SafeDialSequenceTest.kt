541edff10d66bd87d377b8cc002b0538ba7c8adf
Sam Lindsay-Levine <slindsaylevine+github@gmail.com>
2025-12-01 15:32:03 -0600

    2025, Day 1, Part 2: SafeDialSequence clickPassword().

--- src/test/kotlin/advent/year2025/day1/SafeDialSequenceTest.kt
+++ src/test/kotlin/advent/year2025/day1/SafeDialSequenceTest.kt
@@ -25,4 +25,77 @@ class SafeDialSequenceTest {
 
         assertThat(password).isEqualTo(3)
     }
+
+    @Test
+    fun `clickPassword -- sequence with one R1000 -- 10`() {
+        val sequence = SafeDialSequence("R1000")
+
+        val password = sequence.clickPassword()
+
+        assertThat(password).isEqualTo(10)
+    }
+
+    @Test
+    fun `clickPassword -- sequence with one R50 -- 1`() {
+        val sequence = SafeDialSequence("R50")
+
+        val password = sequence.clickPassword()
+
+        assertThat(password).isEqualTo(1)
+    }
+
+    @Test
+    fun `clickPassword -- sequence with one R150 -- 2`() {
+        val sequence = SafeDialSequence("R150")
+
+        val password = sequence.clickPassword()
+
+        assertThat(password).isEqualTo(2)
+    }
+
+    @Test
+    fun `clickPassword -- L50 -- 1`() {
+        val sequence = SafeDialSequence("L50")
+
+        val password = sequence.clickPassword()
+
+        assertThat(password).isEqualTo(1)
+    }
+
+    @Test
+    fun `clickPassword -- L50, L1, R1, L1, R1 -- 3`() {
+        val input = """
+            L50
+            L1
+            R1
+            L1
+            R1
+        """.trimIndent()
+        val sequence = SafeDialSequence(input)
+
+        val password = sequence.clickPassword()
+
+        assertThat(password).isEqualTo(3)
+    }
+
+    @Test
+    fun `clickPassword -- reference input -- 6`() {
+        val input = """
+            L68
+            L30
+            R48
+            L5
+            R60
+            L55
+            L1
+            L99
+            R14
+            L82
+        """.trimIndent()
+        val sequence = SafeDialSequence(input)
+
+        val password = sequence.clickPassword()
+
+        assertThat(password).isEqualTo(6)
+    }
 }
