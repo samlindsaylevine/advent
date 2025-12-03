51d91c112a453e8304fabc42446da6881c851005
Sam Lindsay-Levine <slindsaylevine+github@gmail.com>
2025-12-03 09:00:24 -0600

    2025, Day 3, Part 2: Recursive joltage calculation for BatteryBanks.

--- src/test/kotlin/advent/year2025/day3/BatteryBanksTest.kt
+++ src/test/kotlin/advent/year2025/day3/BatteryBanksTest.kt
@@ -14,10 +14,25 @@ class BatteryBanksTest {
     "234234234234278, 78",
     "818181911112111, 92"
   )
-  fun `largest joltage -- reference examples -- reference values`(input: String, expected: Int) {
+  fun `largest joltage -- reference examples, 2 digits -- reference values`(input: String, expected: Long) {
     val batteryBank = BatteryBank(input)
 
-    val joltage = batteryBank.largestJoltage()
+    val joltage = batteryBank.largestJoltage(2)
+
+    assertThat(joltage).isEqualTo(expected)
+  }
+
+  @ParameterizedTest
+  @CsvSource(
+    "987654321111111, 987654321111",
+    "811111111111119, 811111111119",
+    "234234234234278, 434234234278",
+    "818181911112111, 888911112111"
+  )
+  fun `largest joltage -- reference examples, 12 digits -- reference values`(input: String, expected: Long) {
+    val batteryBank = BatteryBank(input)
+
+    val joltage = batteryBank.largestJoltage(12)
 
     assertThat(joltage).isEqualTo(expected)
   }
@@ -34,6 +49,21 @@ class BatteryBanksTest {
 
     val joltage = banks.totalOutputJoltage()
 
-    assertThat(joltage).isEqualTo(357)
+    assertThat(joltage).isEqualTo(357L)
+  }
+
+  @Test
+  fun `total output joltage -- reference example, 12 digits -- 3121910778619`() {
+    val input = """
+      987654321111111
+      811111111111119
+      234234234234278
+      818181911112111
+    """.trimIndent()
+    val banks = BatteryBanks(input)
+
+    val joltage = banks.totalOutputJoltage(numDigits = 12)
+
+    assertThat(joltage).isEqualTo(3121910778619L)
   }
 }
